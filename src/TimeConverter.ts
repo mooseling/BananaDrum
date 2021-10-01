639144e4bdd27cf1f9df1c49b86353f8c679e344
James Stevenson <mooseling@hotmail.com>
2021-10-01 15:30:03 +0100

    [TimeConverter] Write whole class

---
+++ src/TimeConverter.ts
@@ -0,0 +1,64 @@
+// Timing strings look like this: 2.3.1
+// That format is bars.beats.16ths
+//   Note that if the beat-unit (the denominator of the time signature)
+//   if 16 or greater, the third value of the timing string will always be 1
+// It can continue to smaller time divisions: bars.beats.16ths.64ths.256ths...
+// This instant at the beginning of a song is 1.1.1
+// All trailing .1's can be chopped though, so the timing string will just be '1'
+
+// For triplets, the timing will look like this: 2.3.2T, or 4.1TT
+// Each T indicates adding a third of the real time of the timing at the level it appears
+// It is confusing.
+
+// Assumption: tempo is quarter-notes per minute, no matter the time signature
+export function TimeConverter(timeSignature:string, tempo:number) {
+  const [beatsPerBar, beatUnit] = timeSignature.split('/').map(stringValue => Number(stringValue));
+
+  // Lay some ground work...
+  const quarterNotesPerBeat = 4 / beatUnit;
+  const quarterNotesPerBar = quarterNotesPerBeat * beatsPerBar;
+
+  // And produce our actually useful values
+  const secondsPerBar = (quarterNotesPerBar / tempo) * 60;
+  const secondsPerQuarterNote = 60 / tempo;
+  const secondsPerBeat = secondsPerQuarterNote * quarterNotesPerBeat;
+
+  return {convertToRealTime};
+
+
+  function convertToRealTime(timing:string): number {
+    let seconds = 0;
+    const multipliers = timing.split('.').map(convertToMultiplier);
+
+    const barMultiplier = multipliers[0];
+    seconds += barMultiplier * secondsPerBar;
+
+    const beatMultipler = multipliers[1]; // Usually quarter-beats
+    if (beatMultipler)
+      seconds += beatMultipler * secondsPerBeat;
+
+    for (let timingLevel = 2; timingLevel < multipliers.length; timingLevel++) {
+      const multiplier = multipliers[timingLevel];
+      // secondsPerNthNote = (secondsPerQuarterNote * 4) / (4**timingLevel)
+      // n = 16 at timingLevel = 2, 64 at 3, 256 at 4, ...
+      const secondsPerNthNote = secondsPerQuarterNote / (4**(timingLevel - 1));
+      seconds += multiplier * secondsPerNthNote;
+    }
+
+    return seconds;
+  }
+}
+
+// timingBit looks like '2', '3T', or '1TT'
+// The T's indicate adding some triplet spice to this note, so adding thirds
+// The base value is 1 at time=0, so we have to subtract 1
+function convertToMultiplier(timingBit: string): number {
+  const split = timingBit.split('T');
+  const baseMultiplier = Number(split[0]) - 1; // Because we start on the 1
+  if (split.length === 1)
+    return baseMultiplier;
+  if (split.length === 2)
+    return baseMultiplier + 0.333333333333333333333;
+  if (split.length === 3)
+    return baseMultiplier + 0.666666666666666666666;
+}
