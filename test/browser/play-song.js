ba4e2a9f4e63de5949a94513f6b657051f23a621
James Stevenson <mooseling@hotmail.com>
2021-10-02 13:12:09 +0100

    [Tests] Update browser demo to use Library

--- test/browser/play-song.js
+++ test/browser/play-song.js
@@ -1,7 +1,10 @@
 import {exampleArrangement} from '../lib/example-arrangement.js';
+import {instrumentCollection} from '../lib/example-instruments.js';
 import {ArrangementPlayer} from '../../dist/ArrangementPlayer.js';
+import * as Library from '../../dist/Library.js';
 
 document.addEventListener('click', () => {
+  Library.load(instrumentCollection);
   const arrangementPlayer = new ArrangementPlayer();
   arrangementPlayer.load(exampleArrangement);
   arrangementPlayer.play();
