8f4aee4dd1a046441ccc2ef61cacdbfd0741b8f8
James Stevenson <mooseling@hotmail.com>
2021-10-05 13:31:08 +0100

    [Library] [AudioGetter] Change export styles to single items

--- src/prod/AudioGetter.ts
+++ src/prod/AudioGetter.ts
@@ -2,8 +2,10 @@
 // Later we will probably want to move this to a config file
 let path = 'sounds';
 
-export async function get(filename: string): Promise<ArrayBuffer> {
-  const filepath = `${path}/${filename}`;
-  const response = await fetch(filepath);
-  return response.arrayBuffer();
-}
+export const AudioGetter:AudioGetter = {
+  async get(filename: string): Promise<ArrayBuffer> {
+    const filepath = `${path}/${filename}`;
+    const response = await fetch(filepath);
+    return response.arrayBuffer();
+  }
+};
--- src/prod/Library.ts
+++ src/prod/Library.ts
@@ -8,72 +8,76 @@ const loadPromise: Promise<void> = new Promise(resolve => loadPromiseResolver = 
 
 
 
-// ==================================================================
-//                          Public Functions
-// ==================================================================
 
-export function load(libraryToLoad:InstrumentCollection) {
-  if (!libraryLoaded) {
-    populateLibrary(libraryToLoad);
-    loadAllAudio(); // This will eventually cause loadPromise to resolve
+export function Library():Library {
+  return {load, getAudio};
+
+  // ==================================================================
+  //                          Public Functions
+  // ==================================================================
+  function load(libraryToLoad:InstrumentCollection) {
+    if (!libraryLoaded) {
+      populateLibrary(libraryToLoad);
+      loadAllAudio(); // This will eventually cause loadPromise to resolve
+    }
+    return loadPromise;
   }
-  return loadPromise;
-}
 
 
-export function getAudio(instrumentId:string, styleId:string): ArrayBuffer {
-  if (!libraryLoaded)
-    throw 'Trying to get audio from Library before loading the library!';
+  function getAudio(instrumentId:string, styleId:string): ArrayBuffer {
+    if (!libraryLoaded)
+      throw 'Trying to get audio from Library before loading the library!';
 
-  const noteStyle = instruments?.[instrumentId].noteStyles?.[styleId];
-  if (!noteStyle)
-    throw `Missing instrument (${instrumentId}) or style (${styleId})`;
+    const noteStyle = instruments?.[instrumentId].noteStyles?.[styleId];
+    if (!noteStyle)
+      throw `Missing instrument (${instrumentId}) or style (${styleId})`;
 
-  return noteStyle.audio;
-}
+    return noteStyle.audio;
+  }
 
 
 
 
-// ==================================================================
-//                          Private Functions
-// ==================================================================
+  // ==================================================================
+  //                          Private Functions
+  // ==================================================================
 
-function populateLibrary(libraryToLoad:InstrumentCollection): void {
-  for (const instrumentId in libraryToLoad) {
-    const instrument = libraryToLoad[instrumentId];
-    instruments[instrumentId] = {
-      displayName: instrument.displayName,
-      noteStyles: unpackNoteStyles(instrument)
+  function populateLibrary(libraryToLoad:InstrumentCollection): void {
+    for (const instrumentId in libraryToLoad) {
+      const instrument = libraryToLoad[instrumentId];
+      instruments[instrumentId] = {
+        displayName: instrument.displayName,
+        noteStyles: unpackNoteStyles(instrument)
+      }
     }
   }
-}
 
 
-function unpackNoteStyles(instrument:Instrument): NoteStyleSet {
-  const noteStyles:NoteStyleSet = {};
-  for (const styleId in instrument.noteStyles) {
-    noteStyles[styleId] = {
-      file: instrument.noteStyles[styleId].file
-    };
-  }
-  return noteStyles;
-}
-
-
-function loadAllAudio(): void {
-  const audioPromises: Promise<ArrayBuffer>[] = [];
-  for (const instrumentId in instruments) {
-    const instrument = instruments[instrumentId];
+  function unpackNoteStyles(instrument:Instrument): NoteStyleSet {
+    const noteStyles:NoteStyleSet = {};
     for (const styleId in instrument.noteStyles) {
-      const noteStyle = instrument.noteStyles[styleId];
-      const audioPromise = AudioGetter.get(noteStyle.file);
-      audioPromises.push(audioPromise);
-      audioPromise.then(audio => noteStyle.audio = audio);
+      noteStyles[styleId] = {
+        file: instrument.noteStyles[styleId].file
+      };
     }
+    return noteStyles;
   }
-  Promise.all(audioPromises).then(() => {
-    loadPromiseResolver();
-    libraryLoaded = true;
-  });
+
+
+  function loadAllAudio(): void {
+    const audioPromises: Promise<ArrayBuffer>[] = [];
+    for (const instrumentId in instruments) {
+      const instrument = instruments[instrumentId];
+      for (const styleId in instrument.noteStyles) {
+        const noteStyle = instrument.noteStyles[styleId];
+        const audioPromise = AudioGetter.get(noteStyle.file);
+        audioPromises.push(audioPromise);
+        audioPromise.then(audio => noteStyle.audio = audio);
+      }
+    }
+    Promise.all(audioPromises).then(() => {
+      loadPromiseResolver();
+      libraryLoaded = true;
+    });
+  }
 }
