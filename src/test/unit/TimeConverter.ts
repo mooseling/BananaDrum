6ff6e778223f55c5952756bc64faa44f724668a0
James Stevenson <mooseling@hotmail.com>
2021-10-08 17:56:13 +0100

    [TimeConverter] |Tests| Add triplet test cases

--- src/test/unit/TimeConverter.ts
+++ src/test/unit/TimeConverter.ts
@@ -18,11 +18,27 @@ describe('TimeConverter', function() {
       ['17', 32],
       ['17.1.1.1.1.1', 32],
       ['17.2', 32.5],
-      ['19.2.4', 36.875],
       ['19.2.4', 36.875]
     ];
-    it('Passes a bunch of test cases', () => {
+    it('passes a bunch of test cases', () => {
       testCases.forEach(([timing, expectedRealTime]) => {
+        const calculatedRealTime = basicTimeConverter.convertToRealTime(timing);
+        const difference = Math.abs(expectedRealTime - calculatedRealTime);
+        if (difference > 0.00000001)
+          throw `${timing} converted to ${calculatedRealTime} but expected ${expectedRealTime}`;
+      });
+    });
+
+    const tripletTestCases:TestCase[] = [
+      ['1.2T', 2/3], // 2 seconds per bar at 120bpm
+      ['1.3TT', 4/3],
+      ['1.1T', 1/6],
+      ['4.2T', 6 + 2/3],
+      ['7.3TT', 12 + 4/3],
+      ['2.1T', 2 + 1/6]
+    ];
+    it('...and a bunch of test cases with triplets', () => {
+      tripletTestCases.forEach(([timing, expectedRealTime]) => {
         const calculatedRealTime = basicTimeConverter.convertToRealTime(timing);
         const difference = Math.abs(expectedRealTime - calculatedRealTime);
         if (difference > 0.00000001)
@@ -42,10 +58,26 @@ describe('TimeConverter', function() {
       ['3.3', 3],
       ['3.3.1.1.1.1', 3],
       ['47.5.1', 60],
-      ['47.5', 60]
+      ['47.5', 60],
+      ['8', 9]
     ];
     it('Passes a bunch of test cases', () => {
       testCases.forEach(([timing, expectedRealTime]) => {
+        const calculatedRealTime = basicTimeConverter.convertToRealTime(timing);
+        const difference = Math.abs(expectedRealTime - calculatedRealTime);
+        if (difference > 0.00000001)
+          throw `${timing} converted to ${calculatedRealTime} but expected ${expectedRealTime}`;
+      });
+    });
+
+    const tripletTestCases:TestCase[] = [
+      ['1.5.2T', 1],
+      ['2.4.1TT', 2],
+      ['8.5.2T', 10],
+      ['9.4.1TT', 11]
+    ];
+    it('...and a bunch of test cases with triplets', () => {
+      tripletTestCases.forEach(([timing, expectedRealTime]) => {
         const calculatedRealTime = basicTimeConverter.convertToRealTime(timing);
         const difference = Math.abs(expectedRealTime - calculatedRealTime);
         if (difference > 0.00000001)
