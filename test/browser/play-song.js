c744d04b33eb6b20513c52ed8d89719421aaa7b9
James Stevenson <mooseling@hotmail.com>
2021-10-08 14:14:54 +0100

    [Tests] Add play and pause buttons to browser demo

--- test/browser/play-song.js
+++ test/browser/play-song.js
@@ -1,13 +1,22 @@
+/* jshint browser:true */
 import {exampleArrangement} from '../../dist/test/lib/example-arrangement.js';
 import {instrumentCollection} from '../../dist/test/lib/example-instruments.js';
 import {ArrangementPlayer} from '../../dist/prod/ArrangementPlayer.js';
 import {Library} from '../../dist/prod/Library.js';
 const library = Library();
 
-document.addEventListener('click', () => {
+document.getElementById('load-button').addEventListener('click', function() {
   library.load(instrumentCollection).then(() => {
     const arrangementPlayer = new ArrangementPlayer(library, exampleArrangement);
     arrangementPlayer.loop();
-    arrangementPlayer.play();
+    const playButton = document.getElementById('play-button');
+    const pauseButton = document.getElementById('pause-button');
+
+    playButton.addEventListener('click', () => arrangementPlayer.play());
+    pauseButton.addEventListener('click', () => arrangementPlayer.pause());
+    playButton.style.display = '';
+    pauseButton.style.display = '';
+
+    this.remove();
   });
 });
