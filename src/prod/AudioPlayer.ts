84cdc760cc26047f419fa7482b35e568d8b11588
James Stevenson <mooseling@hotmail.com>
2021-10-20 16:05:41 +0100

    [AudioPlayer] Implement new design

--- src/prod/AudioPlayer.ts
+++ src/prod/AudioPlayer.ts
@@ -1,10 +1,9 @@
 const lookahead = 0.25; // (s) Look 250ms ahead for notes
 const loopFrequency = 125 // (ms) - mixed units are confusing, but we can skip some pointless maths
 
-export function AudioPlayer(noteSource: NoteSource) {
+export function AudioPlayer(noteEventSource: NoteEventSource): AudioPlayer {
   const audioContext = new AudioContext();
   audioContext.suspend();
-  const audioBufferCache = {};
   let nextIteration: number|null = null;
 
   return {play, pause};
@@ -40,49 +39,22 @@ export function AudioPlayer(noteSource: NoteSource) {
 
   function loop() {
     const currentTime = audioContext.currentTime;
-    const notesToSchedule = noteSource.getPlayableNotes(currentTime, currentTime + lookahead);
+    const interval:Interval = {start:currentTime, end: currentTime + lookahead};
+    const notesToSchedule = noteEventSource.getNoteEvents(interval);
     notesToSchedule.forEach(note => scheduleNote(note));
     nextIteration = setTimeout(loop, loopFrequency);
   }
 
-  async function scheduleNote(playableNote: PlayableNote) {
-    const sourceNode = await getSourceNode(playableNote.note);
+  async function scheduleNote(noteEvent: NoteEvent) {
+    const sourceNode = await getSourceNode(noteEvent.note);
     sourceNode.connect(audioContext.destination);
-    sourceNode.start(playableNote.realTime);
+    sourceNode.start(noteEvent.realTime);
   }
 
   async function getSourceNode(note: Note) {
     return new AudioBufferSourceNode(
       audioContext,
-      {buffer: await getAudioBuffer(note)}
+      {buffer: note.audioBuffer}
     );
-  }
-
-  async function getAudioBuffer(note: Note) {
-    const cachedAudioBuffer = getFromCache(note);
-    if (cachedAudioBuffer instanceof Promise)
-      return await cachedAudioBuffer;
-    if (cachedAudioBuffer)
-      return cachedAudioBuffer;
-
-    const audio = noteSource.library.getAudio(note.instrumentId, note.styleId);
-    const audioBufferPromise = audioContext.decodeAudioData(audio);
-    cache(note, audioBufferPromise);
-    const newAudioBuffer = await audioBufferPromise;
-    cache(note, newAudioBuffer);
-
-    return newAudioBuffer;
-  }
-
-  function getFromCache(note: Note): AudioBuffer|Promise<AudioBuffer>|undefined {
-    const {instrumentId, styleId} = note;
-    return audioBufferCache[instrumentId]?.[styleId];
-  }
-
-  function cache(note:Note, thingToCache: AudioBuffer|Promise<AudioBuffer>): void {
-    const {instrumentId, styleId} = note;
-    if (!audioBufferCache[instrumentId])
-      audioBufferCache[instrumentId] = {};
-    audioBufferCache[instrumentId][styleId] = thingToCache;
   }
 }
