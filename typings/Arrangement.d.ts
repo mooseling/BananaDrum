87dff2b729d0a48e8e697cb4d0317693537745b0
James Stevenson <mooseling@hotmail.com>
2021-10-20 15:55:34 +0100

    [Arrangement] Create class

---
+++ typings/Arrangement.d.ts
@@ -0,0 +1,32 @@
+declare interface ArrangementBuilder {
+  (library:Library, packedArrangement?:PackedArrangement): Arrangement
+}
+
+declare interface ArrangementDetails {
+  timeSignature: string,
+  tempo: number,
+  length: number
+}
+
+declare interface Track {
+  instrument: Instrument
+  notes: Note[]
+}
+
+declare interface Arrangement extends ArrangementDetails {
+  tracks: Track[]
+}
+
+declare interface PackedArrangement extends ArrangementDetails {
+  packedTracks: PackedTrack[]
+}
+
+declare interface PackedTrack {
+  instrumentId: string
+  packedNotes: PackedNote[]
+}
+
+declare interface PackedNote {
+  noteStyleId:string
+  timing:string
+}
