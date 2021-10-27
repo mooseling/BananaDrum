9126eba17f9f821dc950c1f15d682624f68144be
James Stevenson <mooseling@hotmail.com>
2021-10-27 15:25:42 +0100

    [Test Lib] Convert example-player creator to example-ecosytem creator

--- src/prod/index.tsx
+++ src/prod/index.tsx
@@ -1,7 +1,7 @@
 import {ArrangementViewer} from './components/ArrangementViewer';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {createExamplePlayer} from '../test/lib/example-player';
+import {createTestEcosystem} from '../test/lib/createTestEcosystem';
 
 // Set React to global so we don't have to import it in every file with JSX
 // A benefit of this is to supress TS messages about unused var React
@@ -9,7 +9,7 @@ window.React = React;
 
 
 document.getElementById('load-button').addEventListener('click', function() {
-  createExamplePlayer().then(({arrangement, arrangementPlayer}) => {
+  createTestEcosystem().then(({arrangement, arrangementPlayer}) => {
 
     ReactDOM.render(<ArrangementViewer arrangement={arrangement}/>, document.getElementById('wrapper'));
     arrangementPlayer.loop();
---
+++ src/test/lib/createTestEcosystem.ts
@@ -0,0 +1,19 @@
+import {exampleArrangement} from './example-arrangement';
+import {instrumentCollection} from './example-instruments';
+import {Library} from '../../prod/Library';
+import {Arrangement} from '../../prod/Arrangement';
+import {ArrangementPlayer} from '../../prod/ArrangementPlayer';
+
+type Ecosystem = {
+  library: Library,
+  arrangement: Arrangement,
+  arrangementPlayer: ArrangementPlayer
+};
+
+export async function createTestEcosystem(): Promise<Ecosystem> {
+  const library = Library(instrumentCollection);
+  await library.load();
+  const arrangement = Arrangement(library, exampleArrangement);
+  const arrangementPlayer = ArrangementPlayer(arrangement);
+  return {library, arrangement, arrangementPlayer};
+}
