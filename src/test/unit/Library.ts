6bba4cd33f40f4dd3f42f13504bf36bca597ad15
James Stevenson <mooseling@hotmail.com>
2021-10-28 20:00:07 +0100

    [Library] |Tests| Update tests to match design

--- src/test/unit/Library.ts
+++ src/test/unit/Library.ts
@@ -1,7 +1,6 @@
 import {assert} from 'chai';
 import {Library} from '../../prod/Library';
 import {instrumentCollection} from '../lib/example-instruments';
-import * as log from '../lib/logging';
 
 const library = Library(instrumentCollection);
 
@@ -22,13 +21,18 @@ describe('Loaded Library', function() {
 
   it('has instruments', () => assert(Object.keys(library.instruments).length > 0));
 
-  it('those instruments can create untimed-notes with AudioBuffers', () => {
+  it('...which have note-styles', () => {
     for (const instrumentId in library.instruments) {
       const instrument = library.instruments[instrumentId];
-      for (const noteStyleId in instrument.noteStyles) {
-        const untimedNote:UntimedNote = instrument.createUntimedNote(noteStyleId);
-        assert(untimedNote.audioBuffer instanceof AudioBuffer);
-      }
+      assert(Object.keys(instrument.noteStyles).length);
+    }
+  });
+
+  it('...which have AudioBuffers', () => {
+    for (const instrumentId in library.instruments) {
+      const instrument = library.instruments[instrumentId];
+      for (const noteStyleId in instrument.noteStyles)
+        assert(instrument.noteStyles[noteStyleId].audioBuffer);
     }
   });
 });
