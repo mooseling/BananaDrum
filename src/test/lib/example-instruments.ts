ec327a86b7fd96d8a630bd3fcb7947ef8eee0703
James Stevenson <mooseling@hotmail.com>
2021-11-03 12:08:45 +0000

    [Demo] Change sample pack to full kit

--- src/test/lib/example-instruments.ts
+++ src/test/lib/example-instruments.ts
@@ -1,26 +1,37 @@
 export const instrumentCollection:InstrumentCollection = [
   {
-    instrumentId: 'kick',
-    displayName: 'Kick drum',
+    instrumentId: 'ride',
+    displayName: 'Ride',
     packedNoteStyles: [
       {
-        noteStyleId: 'kick',
-        file: 'kick.mp3'
-        // displayName, symbol
+        noteStyleId: 'tap',
+        file: 'ride_tap.wav'
+      },
+      {
+        noteStyleId: 'crash',
+        file: 'ride_crash.wav'
+      },
+      {
+        noteStyleId: 'bell',
+        file: 'ride_bell.wav'
       }
     ]
   },
   {
-    instrumentId: 'snare',
-    displayName: 'Snare',
+    instrumentId: 'crash',
+    displayName: 'Crash',
     packedNoteStyles: [
       {
-        noteStyleId: 'accent',
-        file: 'snare.mp3'
+        noteStyleId: 'crash',
+        file: 'crash_crash.wav'
       },
       {
-        noteStyleId: 'roll',
-        file: 'roll.mp3'
+        noteStyleId: 'tap',
+        file: 'crash_tap.wav'
+      },
+      {
+        noteStyleId: 'bell',
+        file: 'crash_bell.wav'
       }
     ]
   },
@@ -30,7 +41,98 @@ export const instrumentCollection:InstrumentCollection = [
     packedNoteStyles: [
       {
         noteStyleId: 'closed',
-        file: 'hihat.mp3'
+        file: 'hihat_closed.wav'
+      },
+      {
+        noteStyleId: 'open',
+        file: 'hihat_open.wav'
+      },
+      {
+        noteStyleId: 'foot',
+        file: 'hihat_foot.wav'
+      },
+      {
+        noteStyleId: 'trash',
+        file: 'hihat_trash.wav'
+      }
+    ]
+  },
+  {
+    instrumentId: 'snare',
+    displayName: 'Snare',
+    packedNoteStyles: [
+      {
+        noteStyleId: 'accent',
+        file: 'snare_accent.wav'
+      },
+      {
+        noteStyleId: 'ghost',
+        file: 'snare_ghost.wav'
+      },
+      {
+        noteStyleId: 'rimshot',
+        file: 'snare_rimshot.wav'
+      },
+      {
+        noteStyleId: 'sidestick',
+        file: 'snare_sidestick.wav'
+      }
+    ]
+  },
+  {
+    instrumentId: 'kick',
+    displayName: 'Kick drum',
+    packedNoteStyles: [
+      {
+        noteStyleId: 'accent',
+        file: 'kick_accent.wav'
+      },
+      {
+        noteStyleId: 'ghost',
+        file: 'kick_ghost.wav'
+        // displayName, symbol
+      }
+    ]
+  },
+  {
+    instrumentId: 'hightom',
+    displayName: 'High Tom',
+    packedNoteStyles: [
+      {
+        noteStyleId: 'accent',
+        file: 'hightom_accent.wav'
+      },
+      {
+        noteStyleId: 'ghost',
+        file: 'hightom_ghost.wav'
+      }
+    ]
+  },
+  {
+    instrumentId: 'midtom',
+    displayName: 'Mid Tom',
+    packedNoteStyles: [
+      {
+        noteStyleId: 'accent',
+        file: 'midtom_accent.wav'
+      },
+      {
+        noteStyleId: 'ghost',
+        file: 'midtom_ghost.wav'
+      }
+    ]
+  },
+  {
+    instrumentId: 'floortom',
+    displayName: 'Floor Tom',
+    packedNoteStyles: [
+      {
+        noteStyleId: 'accent',
+        file: 'floortom_accent.wav'
+      },
+      {
+        noteStyleId: 'ghost',
+        file: 'floortom_ghost.wav'
       }
     ]
   }
