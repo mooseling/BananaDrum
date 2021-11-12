241ddd17a5660220a1b93318e6001a5a72bbd4f0
James Stevenson <mooseling@hotmail.com>
2021-11-12 15:54:58 +0000

    [UI] Subscribe ArrangementViewer to TimeParams

--- src/prod/components/ArrangementViewer.tsx
+++ src/prod/components/ArrangementViewer.tsx
@@ -1,7 +1,10 @@
 import {TrackViewer} from './TrackViewer';
 import {ArrangementControls} from './ArrangementControls';
+import {useState, useEffect} from 'react';
 
 export function ArrangementViewer({arrangement}:{arrangement:Arrangement}): JSX.Element {
+  let [state, update] = useState({arrangement});
+  useEffect(() => arrangement.timeParams.subscribe(() => update({arrangement})), []);
   return (
     <div className="arrangement-viewer">
       <ArrangementControls arrangement={arrangement}/>
