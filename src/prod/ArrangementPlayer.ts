cdf8bab30ae2964acedd9e346a13b3fd80081336
James Stevenson <mooseling@hotmail.com>
2021-11-15 12:49:39 +0000

    [ArrangementPlayer] (Breaks) Remove note-history

--- src/prod/ArrangementPlayer.ts
+++ src/prod/ArrangementPlayer.ts
@@ -8,9 +8,6 @@ export function ArrangementPlayer(arrangement:Arrangement): ArrangementPlayer {
   const offsetter = Offsetter(arrangement.timeParams.tempo);
   let timeConverter:TimeConverter = TimeConverter(arrangement.timeParams);
   arrangement.timeParams.subscribe(handleTimeParamsChange);
-  // To prevent playing note-events multiple times,
-  // we keep track of which loops we've played them in
-  const noteHistory = NotePlayHistory();
   let isLooping = false;
 
   return {play, pause, loop};
@@ -61,7 +58,6 @@ export function ArrangementPlayer(arrangement:Arrangement): ArrangementPlayer {
       arrangement.getAudioEvents(loopInterval).forEach(audioEvent => {
         if (!noteHistory.check(audioEvent, loopNumber)) {
           audioEvents.push(adjustAudioEvent(audioEvent, loopNumber));
-          noteHistory.record(audioEvent, loopNumber);
         }
       });
     });
@@ -85,37 +81,7 @@ export function ArrangementPlayer(arrangement:Arrangement): ArrangementPlayer {
     timeConverter = TimeConverter(arrangement.timeParams);
     offsetter.update(arrangement.timeParams.tempo, audioPlayer.getTime());
   }
-}
 
-// To prevent double-playing notes, we keep track of which ones we've already provided
-// We will be moving this functionality down into AudioPlayer soon
-interface NotePlayHistory {
-  record(audioEvent:AudioEvent, loopNumber:number): void
-  check(audioEvent:AudioEvent, loopNumber:number): boolean
-}
 
-function NotePlayHistory():NotePlayHistory {
-  const noteRecords:Map<AudioEvent, number[]> = new Map();
-
-  return {
-    record(audioEvent:AudioEvent, loopNumber:number) {
-      const noteHistory = getOrCreateHistory(audioEvent);
-      noteHistory.push(loopNumber);
-    },
-    check(audioEvent:AudioEvent, loopNumber:number) {
-      const noteHistory = noteRecords.get(audioEvent);
-      if (noteHistory)
-        return noteHistory.includes(loopNumber);
-      return false;
-    }
-  };
-
-  function getOrCreateHistory(audioEvent:AudioEvent) {
-    const existingHistory = noteRecords.get(audioEvent);
-    if (existingHistory)
-      return existingHistory;
-    const noteHistory = [];
-    noteRecords.set(audioEvent, noteHistory);
-    return noteHistory;
   }
 }
