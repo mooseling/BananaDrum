ad19a3dccdd6a604ab0fe47240e7543ea078721d
James Stevenson <mooseling@hotmail.com>
2021-11-19 20:08:31 +0000

    [ArrangementPlayer] Use TimeCoordinator

--- src/prod/ArrangementPlayer.ts
+++ src/prod/ArrangementPlayer.ts
@@ -1,13 +1,9 @@
-import {AudioPlayer} from './AudioPlayer';
-import {Offsetter} from './Offsetter';
-import {TimeConverter} from './TimeConverter';
+import {TimeCoordinator} from './TimeCoordinator';
 import {TrackPlayer} from './TrackPlayer';
 
 
 export function ArrangementPlayer(arrangement:Arrangement): ArrangementPlayer {
-  const offsetter = Offsetter(arrangement.timeParams.tempo);
-  let timeConverter:TimeConverter = TimeConverter(arrangement.timeParams);
-  arrangement.timeParams.subscribe(handleTimeParamsChange);
+  const timeCoordinator = TimeCoordinator(arrangement.timeParams);
   let isLooping = false;
   const trackPlayers:TrackPlayer[] = createTrackPlayers();
 
@@ -28,16 +24,18 @@ export function ArrangementPlayer(arrangement:Arrangement): ArrangementPlayer {
   // If we're looping we'll use TimeConverter to resolve it within loops
   function getAudioEvents(interval:Interval): AudioEvent[] {
     const audioEvents:AudioEvent[] = [];
-    const offsetInterval = offsetter.getInterval(interval);
-
     const loopIntervals:LoopInterval[] = isLooping ?
-      timeConverter.getLoopIntervals(offsetInterval) :
-      [{...interval, loopNumber:0}];
+      timeCoordinator.convertToLoopIntervals(interval) :
+      timeCoordinator.convertToLoopIntervals(interval).filter(({loopNumber}) => loopNumber === 0);
+
     loopIntervals.forEach(loopInterval => {
       const {loopNumber} = loopInterval;
       trackPlayers.forEach(trackPlayer =>
         trackPlayer.getAudioEvents(loopInterval)
-        .forEach(audioEvent => audioEvents.push(adjustAudioEvent(audioEvent, loopNumber)))
+        .forEach(audioEvent => audioEvents.push({
+          ...getIdentifiedAudioEvent(audioEvent, loopNumber),
+          realTime: timeCoordinator.convertToAudioTime(audioEvent.realTime, loopNumber)
+        }))
       );
     });
 
@@ -60,25 +58,11 @@ export function ArrangementPlayer(arrangement:Arrangement): ArrangementPlayer {
 
 
 
-  function adjustAudioEvent(audioEvent:AudioEvent, loopNumber:number): AudioEvent {
-    return {
-      ...getIdentifiedAudioEvent(audioEvent, loopNumber),
-      realTime: offsetter.unoffset(timeConverter.getLoopRealTime(audioEvent.realTime, loopNumber))
-    };
-  }
-
-
   // AudioEvents coming out of arrangements are uniquely identified from the arrangement's perspective
   // We'll extend the identifier so they are uniquely identified within the arrangement-player
   function getIdentifiedAudioEvent(audioEvent:AudioEvent, loopNumber:number): AudioEvent {
     const identifier = `${audioEvent.identifier}--${loopNumber}`;
     return  {...audioEvent, identifier};
-  }
-
-
-  function handleTimeParamsChange() {
-    timeConverter = TimeConverter(arrangement.timeParams);
-    offsetter.update(arrangement.timeParams.tempo, AudioPlayer.getTime());
   }
 
 
