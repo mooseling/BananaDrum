9d6df4f780cc4560d89088ad81022ffbfed48cc6
James Stevenson <mooseling@hotmail.com>
2021-12-01 12:45:25 +0000

    [ArrangementViewer] Highlight current NoteViewer

--- src/prod/components/ArrangementViewer.tsx
+++ src/prod/components/ArrangementViewer.tsx
@@ -1,12 +1,19 @@
 import {TrackViewer} from './TrackViewer';
 import {ArrangementControls} from './ArrangementControls';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, createRef} from 'react';
 
-export function ArrangementViewer({arrangement}:{arrangement:Arrangement}): JSX.Element {
+let lastTiming:Timing = '0';
+let ref;
+
+export function ArrangementViewer({arrangement, arrangementPlayer}:{arrangement:Arrangement, arrangementPlayer:ArrangementPlayer}): JSX.Element {
+  ref = createRef<HTMLDivElement>();
   let [state, update] = useState({arrangement});
   useEffect(() => arrangement.timeParams.subscribe(() => update({arrangement})), []);
+
+  useEffect(() => arrangementPlayer.subscribe(() => highlightTiming(arrangementPlayer.getCurrentTiming(), ref)), []);
+
   return (
-    <div className="arrangement-viewer">
+    <div className="arrangement-viewer" ref={ref}>
       <ArrangementControls arrangement={arrangement}/>
       {arrangement.tracks.map(track => (
         <TrackViewer
@@ -16,4 +23,20 @@ export function ArrangementViewer({arrangement}:{arrangement:Arrangement}): JSX.
       ))}
     </div>
   );
+}
+
+
+
+function highlightTiming(timingToHighlight:Timing, ref:React.RefObject<HTMLDivElement>) {
+  if (timingToHighlight !== lastTiming) {
+    lastTiming = timingToHighlight;
+    const noteViewers:NodeList = ref.current.querySelectorAll('.note-viewer');
+    noteViewers.forEach((noteViewer:Element) => {
+      const timing:Timing = noteViewer.getAttribute('timing');
+      if (timing === timingToHighlight)
+        noteViewer.classList.add('current-timing');
+      else
+        noteViewer.classList.remove('current-timing');
+    });
+  }
 }
