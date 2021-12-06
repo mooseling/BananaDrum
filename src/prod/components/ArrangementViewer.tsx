cf269d74a4e9751726cbb99e1348bd6d93182f41
James Stevenson <mooseling@hotmail.com>
2021-12-06 23:17:34 +0000

    [ArrangementViewer] Split into head and body

--- src/prod/components/ArrangementViewer.tsx
+++ src/prod/components/ArrangementViewer.tsx
@@ -7,17 +7,27 @@ export const ArrangementPlayerContext = createContext(null);
 export function ArrangementViewer({arrangement, arrangementPlayer}:{arrangement:Banana.Arrangement, arrangementPlayer:Banana.ArrangementPlayer}): JSX.Element {
   let [state, update] = useState({arrangement});
   useEffect(() => arrangement.timeParams.subscribe(() => update({arrangement})), []);
+
   return (
     <ArrangementPlayerContext.Provider value={arrangementPlayer}>
       <div className="arrangement-viewer">
-        <ArrangementControls arrangement={arrangement}/>
-        {arrangement.tracks.map(track => (
-          <TrackViewer
-            track={track}
-            key={track.instrument.instrumentId}
-            arrangement={arrangement}/>
-        ))}
+        <div className="arrangement-viewer-head">
+          <ArrangementControls arrangement={arrangement}/>
+        </div>
+        <div className="arrangement-viewer-body">
+          {getTrackViewers(arrangement)}
+        </div>
       </div>
     </ArrangementPlayerContext.Provider>
   );
+}
+
+
+function getTrackViewers(arrangement:Banana.Arrangement) {
+  return arrangement.tracks.map(track => (
+    <TrackViewer
+      track={track}
+      key={track.instrument.instrumentId}
+      arrangement={arrangement}/>
+  ));
 }
