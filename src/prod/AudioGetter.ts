2f6fd3bbcf33306b635580458b9e776cb30fccbd
James Stevenson <mooseling@hotmail.com>
2021-12-07 18:03:21 +0000

    [AudioGetter] Don't worry about AudioContext init

--- src/prod/AudioGetter.ts
+++ src/prod/AudioGetter.ts
@@ -1,15 +1,13 @@
 // Currently this module knows where we keep sound files
 // Later we will probably want to move this to a config file
 const path = 'sounds';
-let ctx: null|AudioContext = null;
+let ctx: AudioContext = new AudioContext();
 
 export const AudioGetter:Banana.AudioGetter = {
   async get(filename: string): Promise<AudioBuffer> {
     const filepath = `${path}/${filename}`;
     const response = await fetch(filepath);
     const arrayBuffer = await response.arrayBuffer();
-    if (ctx === null)
-      ctx = new AudioContext();
     return ctx.decodeAudioData(arrayBuffer);
   }
 };
