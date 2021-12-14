073f40decf685e2c00b65b4f40e557a9b238740f
James Stevenson <mooseling@hotmail.com>
2021-12-14 18:09:46 +0000

    [Demo] Remove empty tracks

--- src/test/lib/example-arrangement.ts
+++ src/test/lib/example-arrangement.ts
@@ -6,13 +6,6 @@ export const exampleArrangement:Banana.PackedArrangement = {
   },
   packedTracks: [
     {
-      instrumentId: 'ride',
-      packedNotes: []
-    },{
-      instrumentId: 'crash',
-      packedNotes: []
-    },
-    {
       instrumentId: 'hihat',
       packedNotes: [{noteStyleId:'closed', timing:'1.1.1'},{noteStyleId:'closed', timing:'1.1.3'},{noteStyleId:'closed', timing:'1.2.1'},{noteStyleId:'closed', timing:'1.2.3'},{noteStyleId:'closed', timing:'1.3.1'},{noteStyleId:'closed', timing:'1.3.3'},{noteStyleId:'closed', timing:'1.4.1'},{noteStyleId:'closed', timing:'1.4.3'}]
     },
@@ -23,18 +16,6 @@ export const exampleArrangement:Banana.PackedArrangement = {
     {
       instrumentId: 'kick',
       packedNotes: [{noteStyleId:'accent', timing:'1.1.1'},{noteStyleId:'accent', timing:'1.3.1'}]
-    },
-    {
-      instrumentId: 'hightom',
-      packedNotes: []
-    },
-    {
-      instrumentId: 'midtom',
-      packedNotes: []
-    },
-    {
-      instrumentId: 'floortom',
-      packedNotes: []
     }
   ]
 };
