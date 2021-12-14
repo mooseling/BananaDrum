42585f751cac59b40bd2aac09d91030e86c04848
James Stevenson <mooseling@hotmail.com>
2021-12-14 17:36:01 +0000

    [ArrangementViewer] Put InstrumentBrowser in overlay

--- src/prod/components/ArrangementViewer.tsx
+++ src/prod/components/ArrangementViewer.tsx
@@ -1,6 +1,7 @@
 import {TrackViewer} from './TrackViewer';
 import {ArrangementControls} from './ArrangementControls';
 import {InstrumentBrowser} from './InstrumentBrowser';
+import {Overlay} from './Overlay';
 import {useState, useEffect, createContext} from 'react';
 
 export const ArrangementPlayerContext = createContext(null);
@@ -8,6 +9,7 @@ export const ArrangementPlayerContext = createContext(null);
 export function ArrangementViewer({arrangementPlayer}:{arrangementPlayer:Banana.ArrangementPlayer}): JSX.Element {
   const {arrangement} = arrangementPlayer;
   const [tracks, setTracks] = useState({...arrangement.tracks});
+  const [instrumentBrowserVisible, toggleBrowser] = useState(false);
   useEffect(() => arrangement.subscribe(() => setTracks({...arrangement.tracks})), []);
 
   return (
@@ -16,11 +18,16 @@ export function ArrangementViewer({arrangementPlayer}:{arrangementPlayer:Banana.
         <div className="arrangement-viewer-head">
           <ArrangementControls />
         </div>
-        <div className="arrangement-viewer-body">
-          {getTrackViewers(tracks)}
+        <div className="arrangement-viewer-body overlay-wrapper">
+          <div>
+            {getTrackViewers(tracks)}
+            <button onClick={() => toggleBrowser(true)}>Add</button>
+          </div>
+          <Overlay visible={instrumentBrowserVisible}>
+            <InstrumentBrowser close={() => toggleBrowser(false)}/>
+          </Overlay>
         </div>
       </div>
-      <InstrumentBrowser />
     </ArrangementPlayerContext.Provider>
   );
 }
