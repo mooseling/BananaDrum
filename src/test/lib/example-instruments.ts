8be7a9b96dfed4053ec25d24182c1cc9d940ca2a
James Stevenson <mooseling@hotmail.com>
2022-02-04 14:34:50 +0000

    [Demo] Add symbols to kit sounds

--- src/test/lib/example-instruments.ts
+++ src/test/lib/example-instruments.ts
@@ -5,15 +5,18 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     packedNoteStyles: [
       {
         noteStyleId: 'tap',
-        file: 'ride_tap.mp3'
+        file: 'ride_tap.mp3',
+        symbol:{src:'icons/x_lower.svg'}
       },
       {
         noteStyleId: 'crash',
-        file: 'ride_crash.mp3'
+        file: 'ride_crash.mp3',
+        symbol:{src:'icons/X.svg'}
       },
       {
         noteStyleId: 'bell',
-        file: 'ride_bell.mp3'
+        file: 'ride_bell.mp3',
+        symbol:{src:'icons/hollow_diamond.svg'}
       }
     ],
     colourGroup: 'yellow'
@@ -24,15 +27,18 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     packedNoteStyles: [
       {
         noteStyleId: 'crash',
-        file: 'crash_crash.mp3'
+        file: 'crash_crash.mp3',
+        symbol:{src:'icons/X.svg'}
       },
       {
         noteStyleId: 'tap',
-        file: 'crash_tap.mp3'
+        file: 'crash_tap.mp3',
+        symbol:{src:'icons/x.svg'}
       },
       {
         noteStyleId: 'bell',
-        file: 'crash_bell.mp3'
+        file: 'crash_bell.mp3',
+        symbol:{src:'icons/hollow_diamond.svg'}
       }
     ],
     colourGroup: 'yellow'
@@ -43,19 +49,23 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     packedNoteStyles: [
       {
         noteStyleId: 'closed',
-        file: 'hihat_closed.mp3'
+        file: 'hihat_closed.mp3',
+        symbol:{src:'icons/+.svg'}
       },
       {
         noteStyleId: 'open',
-        file: 'hihat_open.mp3'
+        file: 'hihat_open.mp3',
+        symbol:{src:'icons/o_lower.svg'}
       },
       {
         noteStyleId: 'foot',
-        file: 'hihat_foot.mp3'
+        file: 'hihat_foot.mp3',
+        symbol:{src:'icons/O.svg'}
       },
       {
         noteStyleId: 'trash',
-        file: 'hihat_trash.mp3'
+        file: 'hihat_trash.mp3',
+        symbol:{src:'icons/X.svg'}
       }
     ],
     colourGroup: 'yellow'
@@ -66,19 +76,23 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     packedNoteStyles: [
       {
         noteStyleId: 'accent',
-        file: 'snare_accent.mp3'
+        file: 'snare_accent.mp3',
+        symbol:{src:'icons/X.svg'}
       },
       {
         noteStyleId: 'ghost',
-        file: 'snare_ghost.mp3'
+        file: 'snare_ghost.mp3',
+        symbol:{src:'icons/x_lower.svg'}
       },
       {
         noteStyleId: 'rimshot',
-        file: 'snare_rimshot.mp3'
+        file: 'snare_rimshot.mp3',
+        symbol:{src:'icons/sun_rise_rays.svg'}
       },
       {
         noteStyleId: 'sidestick',
-        file: 'snare_sidestick.mp3'
+        file: 'snare_sidestick.mp3',
+        symbol:{src:'icons/sidestick.svg'}
       }
     ],
     colourGroup: 'green'
@@ -89,11 +103,13 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     packedNoteStyles: [
       {
         noteStyleId: 'accent',
-        file: 'kick_accent.mp3'
+        file: 'kick_accent.mp3',
+        symbol:{src:'icons/O_closed.svg'}
       },
       {
         noteStyleId: 'ghost',
-        file: 'kick_ghost.mp3'
+        file: 'kick_ghost.mp3',
+        symbol:{src:'icons/o_closed_lower.svg'}
         // displayName, symbol
       }
     ],
@@ -105,11 +121,13 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     packedNoteStyles: [
       {
         noteStyleId: 'accent',
-        file: 'hightom_accent.mp3'
+        file: 'hightom_accent.mp3',
+        symbol:{src:'icons/O_closed.svg'}
       },
       {
         noteStyleId: 'ghost',
-        file: 'hightom_ghost.mp3'
+        file: 'hightom_ghost.mp3',
+        symbol:{src:'icons/O_closed_lower.svg'}
       }
     ],
     colourGroup: 'purple'
@@ -120,11 +138,13 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     packedNoteStyles: [
       {
         noteStyleId: 'accent',
-        file: 'midtom_accent.mp3'
+        file: 'midtom_accent.mp3',
+        symbol:{src:'icons/O_closed.svg'}
       },
       {
         noteStyleId: 'ghost',
-        file: 'midtom_ghost.mp3'
+        file: 'midtom_ghost.mp3',
+        symbol:{src:'icons/O_closed_lower.svg'}
       }
     ],
     colourGroup: 'purple'
@@ -135,11 +155,13 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     packedNoteStyles: [
       {
         noteStyleId: 'accent',
-        file: 'floortom_accent.mp3'
+        file: 'floortom_accent.mp3',
+        symbol:{src:'icons/O_closed.svg'}
       },
       {
         noteStyleId: 'ghost',
-        file: 'floortom_ghost.mp3'
+        file: 'floortom_ghost.mp3',
+        symbol:{src:'icons/O_closed_lower.svg'}
       }
     ],
     colourGroup: 'purple'
