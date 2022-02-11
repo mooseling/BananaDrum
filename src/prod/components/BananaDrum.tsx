7bb85af6b54ab8748c018bc765a9ca66656e8c0c
James Stevenson <mooseling@hotmail.com>
2022-02-11 21:50:47 +0000

    Tweak About and styles

--- src/prod/components/BananaDrum.tsx
+++ src/prod/components/BananaDrum.tsx
@@ -31,10 +31,14 @@ function About({overlayState}:{overlayState:Banana.OverlayState}): JSX.Element {
     <div id="about" className="welcome">
       <img src="images/banana-with-feet.svg" style={{height:"80pt"}}/>
       <h1>Welcome to Banana Drum!</h1>
-      <p>Banana Drum is currently just a drum sequencer, but one day soon it will be an amazing drum sequencer for samba bands! It will have all the drums of the bateria, and it will be easy for anyone to compose songs, grooves, and breaks, and to share them with others.</p>
-      <p><a target="_blank" href="https://jamofalltrades.com">Check out the author</a></p>
-      <p><a target="_blank" href="https://github.com/mooseling/BananaDrum">Check out the code</a></p>
-      <p><a target="_blank" href="https://trello.com/b/f6731Frf/bananadrum">Check out the progress</a></p>
+      <p>Banana Drum is currently just a drum sequencer, but one day soon it will be an amazing drum sequencer for samba bands! It will have all the drums of the bateria, and it will be easy for anyone to compose songs, grooves, and breaks, and to share them with their bands.</p>
+      <p>
+        <a target="_blank" href="https://jamofalltrades.com">Check out the author</a>
+        <br/>
+        <a target="_blank" href="https://github.com/mooseling/BananaDrum">Check out the code</a>
+        <br/>
+        <a target="_blank" href="https://trello.com/b/f6731Frf/bananadrum">Check on progress</a>
+      </p>
       <button id="load-button" className="push-button" onClick={() => overlayState.toggle()}>Back to my beat!</button>
     </div>
   );
