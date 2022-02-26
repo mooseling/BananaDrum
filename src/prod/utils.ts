b9046a61065cf09c667bbb0711a3026b33c16861
James Stevenson <mooseling@hotmail.com>
2022-02-26 13:40:49 +0000

    [utils] Create utils file, add isSameTiming()

---
+++ src/prod/utils.ts
@@ -0,0 +1,4 @@
+// Comparing timings is easy, but long winded and mistake-prone
+export function isSameTiming(timing1:Banana.Timing, timing2:Banana.Timing): boolean {
+  return (timing1.bar === timing2.bar) && (timing1.step === timing2.step);
+}
