f96143d7a3391b8694e000fdd6050eaf8b8fcfd4
James Stevenson <mooseling@hotmail.com>
2022-03-30 14:45:23 +0100

    [ArrangementViewer] (Style) Add a type-hint

--- src/prod/components/ArrangementViewer.tsx
+++ src/prod/components/ArrangementViewer.tsx
@@ -71,7 +71,7 @@ export function ArrangementViewer({arrangementPlayer}:{arrangementPlayer:Banana.
 }
 
 
-function getTrackViewers(tracks:{[trackId:string]: Banana.Track}) {
+function getTrackViewers(tracks:{[trackId:string]: Banana.Track}): JSX.Element[] {
   return Object.keys(tracks).map(trackId => (
     <TrackViewer
       track={tracks[trackId]}
