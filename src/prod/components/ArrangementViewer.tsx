f6efae4b8231c9dfc6a88e2d4e6ba8fb05feeaab
James Stevenson <mooseling@hotmail.com>
2022-03-30 14:45:00 +0100

    [UI] Hide/show scroll-shadows when needed

--- src/prod/components/ArrangementViewer.tsx
+++ src/prod/components/ArrangementViewer.tsx
@@ -3,9 +3,11 @@ import {ArrangementControls} from './ArrangementControls';
 import {InstrumentBrowser} from './InstrumentBrowser';
 import {Overlay, OverlayState} from './Overlay';
 import {EventEngine} from '../EventEngine';
-import {useState, useEffect, createContext} from 'react';
+import {useState, useEffect, createContext, useRef} from 'react';
+
 
 export const ArrangementPlayerContext = createContext(null);
+
 
 export function ArrangementViewer({arrangementPlayer}:{arrangementPlayer:Banana.ArrangementPlayer}): JSX.Element {
   const {arrangement} = arrangementPlayer;
@@ -27,6 +29,27 @@ export function ArrangementViewer({arrangementPlayer}:{arrangementPlayer:Banana.
     return () => EventEngine.unsubscribe(eventEngineSubscription);
   }, []);
 
+  // Scroll-shadows over the track-viewers
+  // We need to recalculate these classes when:
+  // -- The arrangement-viewer is first rendered
+  // -- The track-viewer-wrapper scrolls
+  // -- The track-viewer-wrapper resizes
+  // -- Notes are added or removed
+  const ref = useRef();
+  const [scrollShadowClasses, setScrollShadowClasses] = useState('');
+  const updateScrollShadows = () => setScrollShadowClasses(getScrollShadowClasses(ref.current));
+  const timeParamsSubscription = () => setTimeout(updateScrollShadows, 0); // time-out so DOM has updated first
+  useEffect(() => {
+    updateScrollShadows(); // Update on first render
+    const resizeObserver = new ResizeObserver(updateScrollShadows);
+    resizeObserver.observe(ref.current); // Watch for track-viewers-wrapper resize
+    arrangement.timeParams.subscribe(timeParamsSubscription); // Watch for length changes
+    return () => {
+      resizeObserver.unobserve(ref.current);
+      arrangement.timeParams.unsubscribe(timeParamsSubscription);
+    }
+  }, []);
+
   return (
     <ArrangementPlayerContext.Provider value={arrangementPlayer}>
       <div className="arrangement-viewer" event-engine-state={eventEngineState}>
@@ -34,7 +57,7 @@ export function ArrangementViewer({arrangementPlayer}:{arrangementPlayer:Banana.
           <ArrangementControls />
         </div>
         <div className="arrangement-viewer-body overlay-wrapper">
-          <div className="track-viewers-wrapper">
+          <div className={`track-viewers-wrapper ${scrollShadowClasses}`} ref={ref} onScroll={updateScrollShadows}>
             {getTrackViewers(tracks)}
           </div>
           <button id="show-instrument-browser" className="push-button" onClick={() => !overlayState.visible && overlayState.toggle()}>Add Instrument</button>
@@ -55,4 +78,30 @@ function getTrackViewers(tracks:{[trackId:string]: Banana.Track}) {
       key={trackId}
     />
   ));
+}
+
+
+// We need scroll shadows if the note-lines are out of site to either the left or the right
+function getScrollShadowClasses(trackViewersWrapper: HTMLElement): string {
+  const noteLine = trackViewersWrapper?.querySelector('.note-line-wrapper');
+  if (!noteLine)
+    return ''; // In case there are no tracks
+
+  const {left: noteLineLeft, right: noteLineRight} = noteLine.getBoundingClientRect();
+
+  // On the left side, the boundary is the right side of the track-metas
+  const {right: metaRight} = trackViewersWrapper.querySelector('.track-meta').getBoundingClientRect();
+
+  // On the right side, the boundary is right edge of the track-viewers-wrapper
+  const {right: wrapperRight} = trackViewersWrapper.getBoundingClientRect();
+
+  // This works much better with a little bit of tolerance, so we do a little subtraction
+  if (noteLineRight - wrapperRight > 2) {
+    if (metaRight - noteLineLeft > 2)
+      return 'overflowing-left overflowing-right';
+    return 'overflowing-right';
+  }
+  if (metaRight - noteLineLeft > 2)
+    return 'overflowing-left';
+  return '';
 }
