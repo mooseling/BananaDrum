3b4975b51023a33598a9d6bc185350627dc5db0e
James Stevenson <mooseling@hotmail.com>
2022-04-20 12:01:27 +0200

    [TimeParams] Add pulse and stepResolution

--- typings/TimeParams.d.ts
+++ typings/TimeParams.d.ts
@@ -3,6 +3,8 @@ declare namespace Banana {
     timeSignature: string
     tempo: number
     length: number
+    pulse: string
+    stepResolution: number
   }
 
   interface TimeParams extends PackedTimeParams, Subscribable {
