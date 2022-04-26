597914ed82cc33ed44c4be4dafdd3795ccf20fb1
James Stevenson <mooseling@hotmail.com>
2022-04-26 17:36:56 +0100

    [Note] (Design) Add unique ID

--- src/prod/Note.ts
+++ src/prod/Note.ts
@@ -1,10 +1,13 @@
 import {Publisher} from './Publisher';
 
+let noteCount = 0;
+
 function buildNote(track:Banana.Track, timing:Banana.Timing, noteStyle:Banana.NoteStyle|null): Banana.Note {
   const publisher = Publisher();
+  const id = `${++noteCount}`;
 
   return {
-    timing, track, subscribe:publisher.subscribe, unsubscribe:publisher.unsubscribe,
+    id, timing, track, subscribe:publisher.subscribe, unsubscribe:publisher.unsubscribe,
     get noteStyle() {return noteStyle;},
     set noteStyle(newNoteStyle:Banana.NoteStyle|null) {
       noteStyle = newNoteStyle;
--- typings/Note.d.ts
+++ typings/Note.d.ts
@@ -1,5 +1,6 @@
 declare namespace Banana {
   interface Note extends Subscribable {
+    id: string
     timing: Timing
     track: Track
     noteStyle: NoteStyle|null // null means this is a rest
