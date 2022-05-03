c800e3c6ed5c977f773da6bd1f787e21e0aeaa61
James Stevenson <mooseling@hotmail.com>
2022-05-03 14:07:06 +0100

    [TimeCoordinator] (Tests) Refactor/clean up

--- src/test/unit/TimeCoordinator.ts
+++ src/test/unit/TimeCoordinator.ts
@@ -11,6 +11,14 @@ const packedTimeParams68_140:PTP = {timeSignature:'6/8', tempo:140, length:1, pu
 const packedTimeParams54:PTP = {timeSignature:'5/4', tempo:90, length:1, pulse:'1/2', stepResolution:8};
 
 describe('TimeCoordinator', function() {
+
+
+
+  // ==================================================================
+  //                          Time Conversion
+  // ==================================================================
+
+
   describe('Time conversion', function() {
     describe('4/4 time, 120bpm', function() {
       const timeCoordinator = TimeCoordinator(TimeParams(packedTimeParams44));
@@ -28,7 +36,7 @@ describe('TimeCoordinator', function() {
         testCases.forEach(([timing, expectedRealTime]) => {
           const calculatedRealTime = timeCoordinator.convertToRealTime(timing);
           assert(closeEnough(calculatedRealTime, expectedRealTime),
-            `${timing.bar}.${timing.step} converted to ${calculatedRealTime} but expected ${expectedRealTime}`);
+            `${toString(timing)} converted to ${calculatedRealTime} but expected ${expectedRealTime}`);
         });
       });
     });
@@ -52,7 +60,7 @@ describe('TimeCoordinator', function() {
         testCases.forEach(([timing, expectedRealTime]) => {
           const calculatedRealTime = timeCoordinator.convertToRealTime(timing);
           assert(closeEnough(calculatedRealTime, expectedRealTime),
-            `${timing.bar}.${timing.step} converted to ${calculatedRealTime} but expected ${expectedRealTime}`);
+            `${toString(timing)} converted to ${calculatedRealTime} but expected ${expectedRealTime}`);
         });
       });
     });
@@ -76,7 +84,7 @@ describe('TimeCoordinator', function() {
         testCases.forEach(([timing, expectedRealTime]) => {
           const calculatedRealTime = timeCoordinator.convertToRealTime(timing);
           assert(closeEnough(calculatedRealTime, expectedRealTime),
-            `${timing.bar}.${timing.step} converted to ${calculatedRealTime} but expected ${expectedRealTime}`);
+            `${toString(timing)} converted to ${calculatedRealTime} but expected ${expectedRealTime}`);
         });
       });
     });
@@ -104,11 +112,18 @@ describe('TimeCoordinator', function() {
         testCases.forEach(([timing, expectedRealTime]) => {
           const calculatedRealTime = timeCoordinator.convertToRealTime(timing);
           assert(closeEnough(calculatedRealTime, expectedRealTime),
-            `${timing.bar}.${timing.step} converted to ${calculatedRealTime} but expected ${expectedRealTime}`);
+            `${toString(timing)} converted to ${calculatedRealTime} but expected ${expectedRealTime}`);
         });
       });
     });
   });
+
+
+
+    // ==================================================================
+    //                          Pulse Conversion
+    // ==================================================================
+
 
   describe('Pulse conversion', function() {
     describe('4/4 time, 120bpm', function() {
@@ -139,104 +154,104 @@ describe('TimeCoordinator', function() {
         testCases.forEach(([timing, expectedPulses]) => {
           const calculatedPulses = timeCoordinator.convertToPulses(timing);
           assert(closeEnough(calculatedPulses, expectedPulses),
-            `${timing.bar}.${timing.step} converted to ${calculatedPulses} pulses but expected ${expectedPulses}`);
+            `${toString(timing)} converted to ${calculatedPulses} pulses but expected ${expectedPulses}`);
         });
       });
     });
-  });
 
-  describe('6/8 time, 120bpm', function() {
-    const timeCoordinator = TimeCoordinator(TimeParams(packedTimeParams68));
+    describe('6/8 time, 120bpm', function() {
+      const timeCoordinator = TimeCoordinator(TimeParams(packedTimeParams68));
 
-    it('Returns 0 on the 1', () => assert(timeCoordinator.convertToPulses({bar:1, step:1}) === 0));
+      it('Returns 0 on the 1', () => assert(timeCoordinator.convertToPulses({bar:1, step:1}) === 0));
 
-    const testCases:TestCase[] = [
-      [{bar:1, step:2}, 0.33333333333333333],
-      [{bar:1, step:3}, 0.66666666666666666],
-      [{bar:1, step:4}, 1],
-      [{bar:1, step:5}, 1.33333333333333333],
-      [{bar:1, step:6}, 1.66666666666666666],
-      [{bar:2, step:1}, 2],
-      [{bar:2, step:2}, 2.33333333333333333],
-      [{bar:2, step:3}, 2.66666666666666666],
-      [{bar:2, step:4}, 3],
-      [{bar:2, step:5}, 3.33333333333333333],
-      [{bar:2, step:6}, 3.66666666666666666],
-      [{bar:3, step:1}, 4],
-      [{bar:100, step:1}, 198],
-    ];
-    it('Passes a bunch of test cases', () => {
-      testCases.forEach(([timing, expectedPulses]) => {
-        const calculatedPulses = timeCoordinator.convertToPulses(timing);
-        assert(closeEnough(calculatedPulses, expectedPulses),
-          `${timing.bar}.${timing.step} converted to ${calculatedPulses} pulses but expected ${expectedPulses}`);
+      const testCases:TestCase[] = [
+        [{bar:1, step:2}, 0.33333333333333333],
+        [{bar:1, step:3}, 0.66666666666666666],
+        [{bar:1, step:4}, 1],
+        [{bar:1, step:5}, 1.33333333333333333],
+        [{bar:1, step:6}, 1.66666666666666666],
+        [{bar:2, step:1}, 2],
+        [{bar:2, step:2}, 2.33333333333333333],
+        [{bar:2, step:3}, 2.66666666666666666],
+        [{bar:2, step:4}, 3],
+        [{bar:2, step:5}, 3.33333333333333333],
+        [{bar:2, step:6}, 3.66666666666666666],
+        [{bar:3, step:1}, 4],
+        [{bar:100, step:1}, 198],
+      ];
+      it('Passes a bunch of test cases', () => {
+        testCases.forEach(([timing, expectedPulses]) => {
+          const calculatedPulses = timeCoordinator.convertToPulses(timing);
+          assert(closeEnough(calculatedPulses, expectedPulses),
+            `${toString(timing)} converted to ${calculatedPulses} pulses but expected ${expectedPulses}`);
+        });
       });
     });
-  });
 
-  // Should all come out the same as 6/8 at 120
-  describe('6/8 time, 140bpm', function() {
-    const timeCoordinator = TimeCoordinator(TimeParams(packedTimeParams68_140));
+    // Should all come out the same as 6/8 at 120
+    describe('6/8 time, 140bpm', function() {
+      const timeCoordinator = TimeCoordinator(TimeParams(packedTimeParams68_140));
 
-    it('Returns 0 on the 1', () => assert(timeCoordinator.convertToPulses({bar:1, step:1}) === 0));
+      it('Returns 0 on the 1', () => assert(timeCoordinator.convertToPulses({bar:1, step:1}) === 0));
 
-    const testCases:TestCase[] = [
-      [{bar:1, step:2}, 0.33333333333333333],
-      [{bar:1, step:3}, 0.66666666666666666],
-      [{bar:1, step:4}, 1],
-      [{bar:1, step:5}, 1.33333333333333333],
-      [{bar:1, step:6}, 1.66666666666666666],
-      [{bar:2, step:1}, 2],
-      [{bar:2, step:2}, 2.33333333333333333],
-      [{bar:2, step:3}, 2.66666666666666666],
-      [{bar:2, step:4}, 3],
-      [{bar:2, step:5}, 3.33333333333333333],
-      [{bar:2, step:6}, 3.66666666666666666],
-      [{bar:3, step:1}, 4],
-      [{bar:100, step:1}, 198],
-    ];
-    it('Passes a bunch of test cases', () => {
-      testCases.forEach(([timing, expectedPulses]) => {
-        const calculatedPulses = timeCoordinator.convertToPulses(timing);
-        assert(closeEnough(calculatedPulses, expectedPulses),
-          `${timing.bar}.${timing.step} converted to ${calculatedPulses} pulses but expected ${expectedPulses}`);
+      const testCases:TestCase[] = [
+        [{bar:1, step:2}, 0.33333333333333333],
+        [{bar:1, step:3}, 0.66666666666666666],
+        [{bar:1, step:4}, 1],
+        [{bar:1, step:5}, 1.33333333333333333],
+        [{bar:1, step:6}, 1.66666666666666666],
+        [{bar:2, step:1}, 2],
+        [{bar:2, step:2}, 2.33333333333333333],
+        [{bar:2, step:3}, 2.66666666666666666],
+        [{bar:2, step:4}, 3],
+        [{bar:2, step:5}, 3.33333333333333333],
+        [{bar:2, step:6}, 3.66666666666666666],
+        [{bar:3, step:1}, 4],
+        [{bar:100, step:1}, 198],
+      ];
+      it('Passes a bunch of test cases', () => {
+        testCases.forEach(([timing, expectedPulses]) => {
+          const calculatedPulses = timeCoordinator.convertToPulses(timing);
+          assert(closeEnough(calculatedPulses, expectedPulses),
+            `${toString(timing)} converted to ${calculatedPulses} pulses but expected ${expectedPulses}`);
+        });
       });
     });
-  });
 
-  describe('5/4 time, 90bpm', function() {
-    const timeCoordinator = TimeCoordinator(TimeParams(packedTimeParams54));
+    describe('5/4 time, 90bpm', function() {
+      const timeCoordinator = TimeCoordinator(TimeParams(packedTimeParams54));
 
-    it('Returns 0 on the 1', () => assert(timeCoordinator.convertToPulses({bar:1, step:1}) === 0));
+      it('Returns 0 on the 1', () => assert(timeCoordinator.convertToPulses({bar:1, step:1}) === 0));
 
-    const testCases:TestCase[] = [
-      [{bar:1, step:2}, 0.25],
-      [{bar:1, step:3}, 0.5],
-      [{bar:1, step:4}, 0.75],
-      [{bar:1, step:5}, 1],
-      [{bar:1, step:6}, 1.25],
-      [{bar:1, step:7}, 1.5],
-      [{bar:1, step:8}, 1.75],
-      [{bar:1, step:9}, 2],
-      [{bar:1, step:10}, 2.25],
-      [{bar:2, step:1}, 2.5],
-      [{bar:2, step:2}, 2.75],
-      [{bar:2, step:3}, 3],
-      [{bar:2, step:4}, 3.25],
-      [{bar:2, step:5}, 3.5],
-      [{bar:2, step:6}, 3.75],
-      [{bar:2, step:7}, 4],
-      [{bar:2, step:8}, 4.25],
-      [{bar:2, step:9}, 4.5],
-      [{bar:2, step:10}, 4.75],
-      [{bar:3, step:1}, 5],
-      [{bar:100, step:1}, 247.5],
-    ];
-    it('Passes a bunch of test cases', () => {
-      testCases.forEach(([timing, expectedPulses]) => {
-        const calculatedPulses = timeCoordinator.convertToPulses(timing);
-        assert(closeEnough(calculatedPulses, expectedPulses),
-          `${timing.bar}.${timing.step} converted to ${calculatedPulses} pulses but expected ${expectedPulses}`);
+      const testCases:TestCase[] = [
+        [{bar:1, step:2}, 0.25],
+        [{bar:1, step:3}, 0.5],
+        [{bar:1, step:4}, 0.75],
+        [{bar:1, step:5}, 1],
+        [{bar:1, step:6}, 1.25],
+        [{bar:1, step:7}, 1.5],
+        [{bar:1, step:8}, 1.75],
+        [{bar:1, step:9}, 2],
+        [{bar:1, step:10}, 2.25],
+        [{bar:2, step:1}, 2.5],
+        [{bar:2, step:2}, 2.75],
+        [{bar:2, step:3}, 3],
+        [{bar:2, step:4}, 3.25],
+        [{bar:2, step:5}, 3.5],
+        [{bar:2, step:6}, 3.75],
+        [{bar:2, step:7}, 4],
+        [{bar:2, step:8}, 4.25],
+        [{bar:2, step:9}, 4.5],
+        [{bar:2, step:10}, 4.75],
+        [{bar:3, step:1}, 5],
+        [{bar:100, step:1}, 247.5],
+      ];
+      it('Passes a bunch of test cases', () => {
+        testCases.forEach(([timing, expectedPulses]) => {
+          const calculatedPulses = timeCoordinator.convertToPulses(timing);
+          assert(closeEnough(calculatedPulses, expectedPulses),
+            `${toString(timing)} converted to ${calculatedPulses} pulses but expected ${expectedPulses}`);
+        });
       });
     });
   });
@@ -246,6 +261,13 @@ describe('TimeCoordinator', function() {
 
 function closeEnough(value1:number, value2:number, threshold = 0.00000001): boolean {
   return Math.abs(value1 - value2) < threshold;
+}
+
+type TimingLike = {bar:number, step:number, score?:number}
+function toString(timing:TimingLike): string {
+  if (timing.score !== undefined)
+    return `${timing.bar}.${timing.step}@${timing.score}`;
+  return `${timing.bar}.${timing.step}`;
 }
 
 // Removed test cases for tempo changes
