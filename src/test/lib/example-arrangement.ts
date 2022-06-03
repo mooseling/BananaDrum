48bb6fe2be46b2b5cabb59662ace0ee654ace0ca
James Stevenson <mooseling@hotmail.com>
2022-06-03 20:39:04 +0100

    Fix example arrangement IDs

--- src/test/lib/example-arrangement.ts
+++ src/test/lib/example-arrangement.ts
@@ -8,16 +8,16 @@ export const exampleArrangement:Banana.PackedArrangement = {
   },
   packedTracks: [
     {
-      instrumentId: 'hihat',
-      packedNotes: [{noteStyleId:'closed', timing:'1:1'},{noteStyleId:'closed', timing:'1:3'},{noteStyleId:'closed', timing:'1:5'},{noteStyleId:'closed', timing:'1:7'},{noteStyleId:'closed', timing:'1:9'},{noteStyleId:'closed', timing:'1:11'},{noteStyleId:'closed', timing:'1:13'},{noteStyleId:'closed', timing:'1:15'},{noteStyleId:'closed', timing:'2:1'},{noteStyleId:'closed', timing:'2:3'},{noteStyleId:'closed', timing:'2:5'},{noteStyleId:'closed', timing:'2:7'},{noteStyleId:'closed', timing:'2:9'},{noteStyleId:'closed', timing:'2:11'},{noteStyleId:'closed', timing:'2:13'},{noteStyleId:'closed', timing:'2:15'}]
+      instrumentId: '2', // hi hat
+      packedNotes: [{noteStyleId:'1', timing:'1:1'},{noteStyleId:'1', timing:'1:3'},{noteStyleId:'1', timing:'1:5'},{noteStyleId:'1', timing:'1:7'},{noteStyleId:'1', timing:'1:9'},{noteStyleId:'1', timing:'1:11'},{noteStyleId:'1', timing:'1:13'},{noteStyleId:'1', timing:'1:15'},{noteStyleId:'1', timing:'2:1'},{noteStyleId:'1', timing:'2:3'},{noteStyleId:'1', timing:'2:5'},{noteStyleId:'1', timing:'2:7'},{noteStyleId:'1', timing:'2:9'},{noteStyleId:'1', timing:'2:11'},{noteStyleId:'1', timing:'2:13'},{noteStyleId:'1', timing:'2:15'}]
     },
     {
-      instrumentId: 'snare',
-      packedNotes: [{noteStyleId:'accent', timing:'1:5'},{noteStyleId:'ghost', timing:'1:10'},{noteStyleId:'accent', timing:'1:15'},{noteStyleId:'ghost', timing:'2:2'},{noteStyleId:'rimshot', timing:'2:5'},{noteStyleId:'accent', timing:'2:13'}],
+      instrumentId: '3', // snare
+      packedNotes: [{noteStyleId:'1', timing:'1:5'},{noteStyleId:'2', timing:'1:10'},{noteStyleId:'1', timing:'1:15'},{noteStyleId:'2', timing:'2:2'},{noteStyleId:'3', timing:'2:5'},{noteStyleId:'1', timing:'2:13'}],
     },
     {
-      instrumentId: 'kick',
-      packedNotes: [{noteStyleId:'accent', timing:'1:1'},{noteStyleId:'ghost', timing:'1:8'},{noteStyleId:'accent', timing:'1:9'},{noteStyleId:'accent', timing:'1:11'},{noteStyleId:'accent', timing:'2:3'},{noteStyleId:'accent', timing:'2:8'},{noteStyleId:'accent', timing:'2:11'},{noteStyleId:'ghost', timing:'2:16'}]
+      instrumentId: '4', // kick
+      packedNotes: [{noteStyleId:'1', timing:'1:1'},{noteStyleId:'2', timing:'1:8'},{noteStyleId:'1', timing:'1:9'},{noteStyleId:'1', timing:'1:11'},{noteStyleId:'1', timing:'2:3'},{noteStyleId:'1', timing:'2:8'},{noteStyleId:'1', timing:'2:11'},{noteStyleId:'2', timing:'2:16'}]
     }
   ]
 };
