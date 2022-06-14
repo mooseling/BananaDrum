5afae3ff93bd8572036a37c2a08a7b3fa5535d1d
James Stevenson <mooseling@hotmail.com>
2022-06-14 17:15:56 +0100

    [Index] Refactor and load from URL

--- src/prod/index.tsx
+++ src/prod/index.tsx
@@ -1,8 +1,10 @@
 import {EventEngine} from './EventEngine';
+import {ArrangementPlayer} from './ArrangementPlayer';
 import {Library} from './Library';
 import {BananaDrum} from './components/BananaDrum';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {instrumentCollection} from '../test/lib/example-instruments';
 import {createTestEcosystem} from '../test/lib/createTestEcosystem';
 import {urlEncodeNumber, urlDecodeNumber, interpretAsBaseN, convertToBaseN, urlEncodeTrack, createTrackFromUrl, urlEncodeArrangement, urlDecodeArrangement} from './compression';
 
@@ -13,21 +15,46 @@ window.React = React;
 
 document.getElementById('load-button').addEventListener('click', function() {
   EventEngine.initialise();
+  this.replaceWith(createLoadingMessage());
 
-  const loadingMessage = document.createElement('div');
-  loadingMessage.id = 'loading-message';
-  loadingMessage.innerText = 'Loading...';
-  this.replaceWith(loadingMessage);
-  createTestEcosystem().then(({arrangement, arrangementPlayer}) => {
+  getArrangementAndPlayer().then(({arrangement, arrangementPlayer}) => {
     EventEngine.connect(arrangementPlayer);
     document.getElementById('welcome').remove();
-    ReactDOM.render(<BananaDrum arrangementPlayer={arrangementPlayer}/>, document.getElementById('wrapper'));
-
+    ReactDOM.render(
+      <BananaDrum arrangementPlayer={arrangementPlayer}/>,
+      document.getElementById('wrapper')
+    );
 
     // Expose some things for testing:
     Object.assign(window, {arrangement});
   });
 });
+
+
+function createLoadingMessage():HTMLDivElement {
+  const loadingMessage = document.createElement('div');
+  loadingMessage.id = 'loading-message';
+  loadingMessage.innerText = 'Loading...';
+  return loadingMessage;
+}
+
+
+async function getArrangementAndPlayer() {
+  const encodedArrangement = getUrlEncodedArrangement();
+  if (encodedArrangement != null) {
+    Library.load(instrumentCollection);
+    const arrangement = urlDecodeArrangement(encodedArrangement);
+    const arrangementPlayer = ArrangementPlayer(arrangement);
+    return {arrangement, arrangementPlayer};
+  }
+  return createTestEcosystem();
+}
+
+
+function getUrlEncodedArrangement(): string|null {
+  const searchParams = new URLSearchParams(window.location.search);
+  return searchParams.get('a');
+}
 
 
 // Expose functions for testing
