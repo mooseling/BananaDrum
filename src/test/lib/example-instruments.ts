2fe71766ff8cbcc49708029cfab5fa5679ca1b49
James Stevenson <mooseling@hotmail.com>
2022-06-18 16:57:41 +0100

    (Sounds) Add skeletons for rest of bateria

--- src/test/lib/example-instruments.ts
+++ src/test/lib/example-instruments.ts
@@ -1,52 +1,42 @@
 export const instrumentCollection:Banana.InstrumentCollection = [
   {
     id: '0',
-    displayName: 'Low Surdo',
-    packedNoteStyles: [
-      {
-        id: '1',
-        file: 'Low_Surdo_Accent.mp3',
-        symbol: {
-          src:'images/icons/O.svg',
-          string: 'accent'
-        }
-      },
-      {
-        id: '2',
-        file: 'Low_Surdo_Muted.mp3',
-        symbol: {
-          src:'images/icons/O_closed.svg',
-          string: 'muted'
-        }
-      }
-    ],
-    colourGroup: 'blue'
+    displayName: 'Agogo',
+    packedNoteStyles: [],
+    colourGroup: 'yellow'
   },
   {
     id: '1',
-    displayName: 'Mid Surdo',
-    packedNoteStyles: [
-      {
-        id: '1',
-        file: 'Mid_Surdo_Accent.mp3',
-        symbol: {
-          src:'images/icons/O.svg',
-          string: 'accent'
-        }
-      },
-      {
-        id: '2',
-        file: 'Mid_Surdo_Muted.mp3',
-        symbol: {
-          src:'images/icons/O_closed.svg',
-          string: 'muted'
-        }
-      }
-    ],
-    colourGroup: 'blue'
+    displayName: 'Chocalho',
+    packedNoteStyles: [],
+    colourGroup: 'yellow'
   },
   {
-    id: '1',
+    id: '2',
+    displayName: 'Tamborim',
+    packedNoteStyles: [],
+    colourGroup: 'orange'
+  },
+  {
+    id: '3',
+    displayName: 'Repanique',
+    packedNoteStyles: [],
+    colourGroup: 'orange'
+  },
+  {
+    id: '4',
+    displayName: 'Caixa',
+    packedNoteStyles: [],
+    colourGroup: 'green'
+  },
+  // {
+  //   id: '5',
+  //   displayName: 'Timba',
+  //   packedNoteStyles: [],
+  //   colourGroup: 'blue'
+  // },
+  {
+    id: '6',
     displayName: 'High Surdo',
     packedNoteStyles: [
       {
@@ -66,6 +56,52 @@ export const instrumentCollection:Banana.InstrumentCollection = [
         }
       }
     ],
-    colourGroup: 'blue'
+    colourGroup: 'purple'
+  },
+  {
+    id: '7',
+    displayName: 'Mid Surdo',
+    packedNoteStyles: [
+      {
+        id: '1',
+        file: 'Mid_Surdo_Accent.mp3',
+        symbol: {
+          src:'images/icons/O.svg',
+          string: 'accent'
+        }
+      },
+      {
+        id: '2',
+        file: 'Mid_Surdo_Muted.mp3',
+        symbol: {
+          src:'images/icons/O_closed.svg',
+          string: 'muted'
+        }
+      }
+    ],
+    colourGroup: 'purple'
+  },
+  {
+    id: '8',
+    displayName: 'Low Surdo',
+    packedNoteStyles: [
+      {
+        id: '1',
+        file: 'Low_Surdo_Accent.mp3',
+        symbol: {
+          src:'images/icons/O.svg',
+          string: 'accent'
+        }
+      },
+      {
+        id: '2',
+        file: 'Low_Surdo_Muted.mp3',
+        symbol: {
+          src:'images/icons/O_closed.svg',
+          string: 'muted'
+        }
+      }
+    ],
+    colourGroup: 'purple'
   },
 ];
