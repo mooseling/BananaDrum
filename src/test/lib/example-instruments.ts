aeb4fd7553cbdc5ba4cab1eb952f1e6bebae14c8
James Stevenson <mooseling@hotmail.com>
2022-06-18 12:46:39 +0100

    (Sounds) Remove drum kit, add surdos

--- src/test/lib/example-instruments.ts
+++ src/test/lib/example-instruments.ts
@@ -1,234 +1,71 @@
 export const instrumentCollection:Banana.InstrumentCollection = [
   {
     id: '0',
-    displayName: 'Ride',
+    displayName: 'Low Surdo',
     packedNoteStyles: [
       {
         id: '1',
-        file: 'ride_tap.mp3',
-        symbol:{
-          src:'images/icons/o_closed_lower.svg',
-          string: 'tap'
-        }
-      },
-      {
-        id: '2',
-        file: 'ride_crash.mp3',
-        symbol:{
-          src:'images/icons/X.svg',
-          string: 'crash'
-        }
-      },
-      {
-        id: '3',
-        file: 'ride_bell.mp3',
-        symbol:{
-          src:'images/icons/hollow_diamond.svg',
-          string: 'bell'
-        }
-      }
-    ],
-    colourGroup: 'yellow'
-  },
-  {
-    id: '1',
-    displayName: 'Crash',
-    packedNoteStyles: [
-      {
-        id: '1',
-        file: 'crash_crash.mp3',
-        symbol:{
-          src:'images/icons/X.svg',
-          string: 'crash'
-        }
-      },
-      {
-        id: '2',
-        file: 'crash_tap.mp3',
-        symbol:{
-          src:'images/icons/o_closed_lower.svg',
-          string: 'tap'
-        }
-      },
-      {
-        id: '3',
-        file: 'crash_bell.mp3',
-        symbol:{
-          src:'images/icons/hollow_diamond.svg',
-          string: 'bell'
-        }
-      }
-    ],
-    colourGroup: 'yellow'
-  },
-  {
-    id: '2',
-    displayName: 'Hi Hat',
-    packedNoteStyles: [
-      {
-        id: '1',
-        file: 'hihat_closed.mp3',
-        symbol:{
-          src:'images/icons/x_lower.svg',
-          string: 'closed'
-        }
-      },
-      {
-        id: '2',
-        file: 'hihat_open.mp3',
-        symbol:{
+        file: 'Low_Surdo_Accent.mp3',
+        symbol: {
           src:'images/icons/O.svg',
-          string: 'open'
-        }
-      },
-      {
-        id: '3',
-        file: 'hihat_foot.mp3',
-        symbol:{
-          src:'images/icons/o_lower.svg',
-          string: 'foot'
-        }
-      },
-      {
-        id: '4',
-        file: 'hihat_trash.mp3',
-        symbol:{
-          src:'images/icons/X.svg',
-          string: 'trash'
-        }
-      }
-    ],
-    colourGroup: 'yellow'
-  },
-  {
-    id: '3',
-    displayName: 'Snare',
-    packedNoteStyles: [
-      {
-        id: '1',
-        file: 'snare_accent.mp3',
-        symbol:{
-          src:'images/icons/X.svg',
           string: 'accent'
         }
       },
       {
         id: '2',
-        file: 'snare_ghost.mp3',
-        symbol:{
-          src:'images/icons/x_lower.svg',
-          string: 'ghost'
-        }
-      },
-      {
-        id: '3',
-        file: 'snare_rimshot.mp3',
-        symbol:{
-          src:'images/icons/sun_rise_rays.svg',
-          string: 'rimshot'
-        }
-      },
-      {
-        id: '4',
-        file: 'snare_sidestick.mp3',
-        symbol:{
-          src:'images/icons/sidestick.svg',
-          string: 'sidestick'
-        }
-      }
-    ],
-    colourGroup: 'green'
-  },
-  {
-    id: '4',
-    displayName: 'Kick drum',
-    packedNoteStyles: [
-      {
-        id: '1',
-        file: 'kick_accent.mp3',
-        symbol:{
+        file: 'Low_Surdo_Muted.mp3',
+        symbol: {
           src:'images/icons/O_closed.svg',
-          string: 'accent'
-        }
-      },
-      {
-        id: '2',
-        file: 'kick_ghost.mp3',
-        symbol:{
-          src:'images/icons/o_closed_lower.svg',
-          string: 'ghost'
+          string: 'muted'
         }
       }
     ],
     colourGroup: 'blue'
   },
   {
-    id: '5',
-    displayName: 'High Tom',
+    id: '1',
+    displayName: 'Mid Surdo',
     packedNoteStyles: [
       {
         id: '1',
-        file: 'hightom_accent.mp3',
-        symbol:{
-          src:'images/icons/O_closed.svg',
+        file: 'Mid_Surdo_Accent.mp3',
+        symbol: {
+          src:'images/icons/O.svg',
           string: 'accent'
         }
       },
       {
         id: '2',
-        file: 'hightom_ghost.mp3',
-        symbol:{
-          src:'images/icons/o_closed_lower.svg',
-          string: 'ghost'
+        file: 'Mid_Surdo_Muted.mp3',
+        symbol: {
+          src:'images/icons/O_closed.svg',
+          string: 'muted'
         }
       }
     ],
-    colourGroup: 'purple'
+    colourGroup: 'blue'
   },
   {
-    id: '6',
-    displayName: 'Mid Tom',
+    id: '1',
+    displayName: 'High Surdo',
     packedNoteStyles: [
       {
         id: '1',
-        file: 'midtom_accent.mp3',
-        symbol:{
-          src:'images/icons/O_closed.svg',
+        file: 'High_Surdo_Accent.mp3',
+        symbol: {
+          src:'images/icons/O.svg',
           string: 'accent'
         }
       },
       {
         id: '2',
-        file: 'midtom_ghost.mp3',
-        symbol:{
-          src:'images/icons/o_closed_lower.svg',
-          string: 'ghost'
+        file: 'High_Surdo_Muted.mp3',
+        symbol: {
+          src:'images/icons/O_closed.svg',
+          string: 'muted'
         }
       }
     ],
-    colourGroup: 'purple'
+    colourGroup: 'blue'
   },
-  {
-    id: '7',
-    displayName: 'Floor Tom',
-    packedNoteStyles: [
-      {
-        id: '1',
-        file: 'floortom_accent.mp3',
-        symbol:{
-          src:'images/icons/O_closed.svg',
-          string: 'accent'
-        }
-      },
-      {
-        id: '2',
-        file: 'floortom_ghost.mp3',
-        symbol:{
-          src:'images/icons/o_closed_lower.svg',
-          string: 'ghost'
-        }
-      }
-    ],
-    colourGroup: 'purple'
-  }
 ];
