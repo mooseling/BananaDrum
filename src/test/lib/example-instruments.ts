9c08da8eb442f613f96f34411782b6798820239e
James Stevenson <mooseling@hotmail.com>
2022-06-19 10:38:44 +0100

    (Sounds) Fill out bateria details

--- src/test/lib/example-instruments.ts
+++ src/test/lib/example-instruments.ts
@@ -2,41 +2,205 @@ export const instrumentCollection:Banana.InstrumentCollection = [
   {
     id: '0',
     displayName: 'Agogo',
-    packedNoteStyles: [],
+    packedNoteStyles: [
+      {
+        id: '1',
+        file: 'Agogo_Low.mp3',
+        symbol: {
+          src:'images/icons/arrow_down.svg',
+          string: 'high'
+        }
+      },
+      {
+        id: '2',
+        file: 'Agogo_High.mp3',
+        symbol: {
+          src:'images/icons/arrow_up.svg',
+          string: 'low'
+        }
+      }
+    ],
     colourGroup: 'yellow'
   },
   {
     id: '1',
     displayName: 'Chocalho',
-    packedNoteStyles: [],
+    packedNoteStyles: [
+      {
+        id: '1',
+        file: 'Chocalho_Accent.mp3',
+        symbol: {
+          src:'images/icons/X.svg',
+          string: 'accent'
+        }
+      },
+      {
+        id: '2',
+        file: 'Chocalho_Ghost.mp3',
+        symbol: {
+          src:'images/icons/x_lower.svg',
+          string: 'ghost'
+        }
+      }
+    ],
     colourGroup: 'yellow'
   },
   {
     id: '2',
     displayName: 'Tamborim',
-    packedNoteStyles: [],
+    packedNoteStyles: [
+      {
+        id: '1',
+        file: 'Tamborim_Accent.mp3',
+        symbol: {
+          src:'images/icons/X.svg',
+          string: 'accent'
+        }
+      },
+      {
+        id: '2',
+        file: 'Tamborim_Ghost.mp3',
+        symbol: {
+          src:'images/icons/x_lower.svg',
+          string: 'ghost'
+        }
+      }
+    ],
     colourGroup: 'orange'
   },
   {
     id: '3',
     displayName: 'Repanique',
-    packedNoteStyles: [],
+    packedNoteStyles: [
+      {
+        id: '1',
+        file: 'Repanique_Center.mp3',
+        symbol: {
+          src:'images/icons/X.svg',
+          string: 'center'
+        }
+      },
+      {
+        id: '2',
+        file: 'Repanique_Edge.mp3',
+        symbol: {
+          src:'images/icons/x_lower.svg',
+          string: 'edge'
+        }
+      },
+      {
+        id: '3',
+        file: 'Repanique_Rimshot.mp3',
+        symbol: {
+          src:'images/icons/sun_rise_rays.svg',
+          string: 'rimshot'
+        }
+      },
+      {
+        id: '4',
+        file: 'Repanique_Rim.mp3',
+        symbol: {
+          src:'images/icons/hollow_diamond.svg',
+          string: 'rim'
+        }
+      },
+      {
+        id: '5',
+        file: 'Repanique_Buzz.mp3',
+        symbol: {
+          src:'images/icons/triple_slash.svg',
+          string: 'buzz'
+        }
+      },
+      {
+        id: '6',
+        file: 'Repanique_Hand.mp3',
+        symbol: {
+          src:'images/icons/hand.svg',
+          string: 'buzz'
+        }
+      },
+      {
+        id: '7',
+        file: 'Repanique_Slap.mp3',
+        symbol: {
+          src:'images/icons/slap.svg',
+          string: 'buzz'
+        }
+      }
+    ],
     colourGroup: 'orange'
   },
   {
     id: '4',
+    displayName: 'Repanique (Whippy Sticks)',
+    packedNoteStyles: [
+      {
+        id: '1',
+        file: 'Repanique_Whippy_Accent.mp3',
+        symbol: {
+          src:'images/icons/X.svg',
+          string: 'accent'
+        }
+      },
+      {
+        id: '2',
+        file: 'Repanique_Whippy_Ghost.mp3',
+        symbol: {
+          src:'images/icons/x_lower.svg',
+          string: 'ghost'
+        }
+      }
+    ],
+    colourGroup: 'orange'
+  },
+  {
+    id: '5',
     displayName: 'Caixa',
-    packedNoteStyles: [],
+    packedNoteStyles: [
+      {
+        id: '1',
+        file: 'Caixa_Accent.mp3',
+        symbol: {
+          src:'images/icons/X.svg',
+          string: 'accent'
+        }
+      },
+      {
+        id: '2',
+        file: 'Caixa_Ghost.mp3',
+        symbol: {
+          src:'images/icons/x_lower.svg',
+          string: 'ghost'
+        }
+      },
+      {
+        id: '3',
+        file: 'Caixa_Buzz.mp3',
+        symbol: {
+          src:'images/icons/triple_slash.svg',
+          string: 'buzz'
+        }
+      },
+      {
+        id: '4',
+        file: 'Caixa_Rimshot.mp3',
+        symbol: {
+          src:'images/icons/sun_rise_rays.svg',
+          string: 'rimshot'
+        }
+      },
+    ],
     colourGroup: 'green'
   },
   // {
-  //   id: '5',
+  //   id: '6',
   //   displayName: 'Timba',
   //   packedNoteStyles: [],
   //   colourGroup: 'blue'
   // },
   {
-    id: '6',
+    id: '7',
     displayName: 'High Surdo',
     packedNoteStyles: [
       {
@@ -59,7 +223,7 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     colourGroup: 'purple'
   },
   {
-    id: '7',
+    id: '8',
     displayName: 'Mid Surdo',
     packedNoteStyles: [
       {
@@ -82,7 +246,7 @@ export const instrumentCollection:Banana.InstrumentCollection = [
     colourGroup: 'purple'
   },
   {
-    id: '8',
+    id: '9',
     displayName: 'Low Surdo',
     packedNoteStyles: [
       {
