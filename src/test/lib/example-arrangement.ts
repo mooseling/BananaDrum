5541015366039479868a9e9c0c89b0d8af4ac118
James Stevenson <mooseling@hotmail.com>
2022-07-05 10:00:04 +0100

    Change example arrangement

--- src/test/lib/example-arrangement.ts
+++ src/test/lib/example-arrangement.ts
@@ -1,1 +1,1 @@
-export const exampleSongString = '4-4.115.1.1-4.16.06Neb.11IOwQ.26N2J.3MoMEP3ME.5Fv8Qor.76jSF.8Hgm.9SLHS';
+export const exampleSongString = '4-4.105.1.1-4.16.06Neb.11IOwQ.26N2K.3auf6au8E.515TP87a.775j_.8HsB.9SLIh';
