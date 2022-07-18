14593c7ea7c17dfa0300671a9c40b4264d3a2dde
James Stevenson <mooseling@hotmail.com>
2022-07-18 22:13:23 +0100

    [HistoryController] Make back button close overlays on mobile

--- src/prod/HistoryController.ts
+++ src/prod/HistoryController.ts
@@ -1,11 +1,22 @@
+import {isMobile} from './isMobile';
+import {anyOverlaysAreOpen, closeAllOverlays} from './components/Overlay';
+
 export const HistoryController = {
   init() {
     addBufferState();
+    let readyToLeave = false;
     window.addEventListener('popstate', () => {
-      if (confirm('You pressed the back button. Leave Banana Drum?')) {
-        window.history.back();
-      } else {
-        window.history.pushState({}, '');
+      // Mobile back button is a natural button for closing overlays
+      if (isMobile && anyOverlaysAreOpen()) {
+        closeAllOverlays();
+        addBufferState();
+      } else if (!readyToLeave) {
+        if (confirm('You pressed the back button. Leave Banana Drum?')) {
+          readyToLeave = true; // Need to set this because back() trigger this again
+          window.history.back();
+        } else {
+          addBufferState();
+        }
       }
     });
   }
