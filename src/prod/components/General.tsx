ad42119d38bf9c522f849c0939124cbefab65b1e
James Stevenson <mooseling@hotmail.com>
2022-11-20 11:35:58 +0000

    Limit number inputs to numeric values during typing

--- src/prod/components/General.tsx
+++ src/prod/components/General.tsx
@@ -5,6 +5,18 @@ export function NumberInput(
 ): JSX.Element {
   const [visibleValue, setVisibleValue] = useState(getValue());
 
+  // To update the input as you type, but not update the model
+  function attemptSetVisibleValue(inputValue:string) {
+    if (inputValue.length === 0)
+      return setVisibleValue('');
+
+    if (!inputValue.charAt(inputValue.length - 1).match(/[0-9]/))
+      return attemptSetVisibleValue(inputValue.substring(0, inputValue.length - 1));
+
+    setVisibleValue(inputValue);
+  }
+
+  // Try to set the model value, which may fail due to validation
   function attemptSet() {
     try {
       setValue(visibleValue);
@@ -18,7 +30,7 @@ export function NumberInput(
     type="text"
     inputMode="numeric"
     pattern="[0-9]*"
-    onChange={event => setVisibleValue(event.target.value)}
+    onChange={event => attemptSetVisibleValue(event.target.value)}
     value={visibleValue}
     onBlur={() => attemptSet()}
     onKeyPress={event => event.key === 'Enter' && attemptSet()}
