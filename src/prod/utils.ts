d49934eb32d09d470c48a668331766c3f5576446
James Stevenson <mooseling@hotmail.com>
2022-12-15 23:24:09 +0000

    [Utils] Move exists function into utils

--- src/prod/utils.ts
+++ src/prod/utils.ts
@@ -2,3 +2,8 @@
 export function isSameTiming(timing1:Banana.Timing, timing2:Banana.Timing): boolean {
   return (timing1.bar === timing2.bar) && (timing1.step === timing2.step);
 }
+
+
+export function exists<T>(value: T | undefined | null): value is T {
+  return value === (value ?? !value);
+}
