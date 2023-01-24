1dd31fbd3e5c5c89008091839fdb30c39e5767a6
James Stevenson <mooseling@hotmail.com>
2023-01-24 22:40:43 +0000

    [Test] (Dev) Abolish tests with mocked globals for now

--- packages/bananadrum-core/src/test/unit/loadAudio.ts
+++ packages/bananadrum-core/src/test/unit/loadAudio.ts
@@ -1,19 +1,19 @@
 /* global fetch */
-import {assert} from 'chai';
-import '../mocks/fetch.js';
-import {loadAudio} from '../../prod/loadAudio.js';
-import * as log from '../lib/logging.js';
+// import {assert} from 'chai';
+// import '../mocks/fetch.js';
+// import {loadAudio} from '../../prod/loadAudio.js';
+// import * as log from '../lib/logging.js';
 
-describe('loadAudio', function() {
-  const filename = 'boop.mp3';
-  const audioPromise = loadAudio(filename);
-  const requestLog = log.get('fetchRequestLog');
-  const latestRequest = requestLog[requestLog.length - 1];
+// describe('loadAudio', function() {
+//   const filename = 'boop.mp3';
+//   const audioPromise = loadAudio(filename);
+//   const requestLog = log.get('fetchRequestLog');
+//   const latestRequest = requestLog[requestLog.length - 1];
 
-  it('requests the correct file', () => assert(latestRequest.endsWith(filename)));
+//   it('requests the correct file', () => assert(latestRequest.endsWith(filename)));
 
-  it('returns a promise for an AudioBuffer', async () => {
-    const audio = await audioPromise;
-    assert(audio instanceof AudioBuffer);
-  });
-});
+//   it('returns a promise for an AudioBuffer', async () => {
+//     const audio = await audioPromise;
+//     assert(audio instanceof AudioBuffer);
+//   });
+// });
--- packages/bananadrum-core/src/test/unit/mocks.ts
+++ packages/bananadrum-core/src/test/unit/mocks.ts
@@ -1,48 +1,48 @@
-import {assert} from 'chai';
-import * as log from '../lib/logging.js';
+// import {assert} from 'chai';
+// import * as log from '../lib/logging.js';
 
 
-describe('Logging', function() {
-  const itemsToLog = [];
-  log.set('logTest', itemsToLog);
-  const returnedVar = log.get('logTest');
+// describe('Logging', function() {
+//   const itemsToLog = [];
+//   log.set('logTest', itemsToLog);
+//   const returnedVar = log.get('logTest');
 
-  it('returns things you set', () => assert(returnedVar === itemsToLog));
+//   it('returns things you set', () => assert(returnedVar === itemsToLog));
 
-  it('is useful for adding things to arrays', () => {
-    itemsToLog[0] = 'boink';
-    assert(returnedVar[0] === 'boink');
-  })
-});
-
-
-describe('WebAudio mock', function() {
-  describe('AudioContext Mock', function() {
-    const ctx = new AudioContext();
-    it('can create AudioBuffers', async () => {
-      const arrayBuffer = new ArrayBuffer(8);
-      const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
-      assert(audioBuffer instanceof AudioBuffer);
-    });
-  });
-});
+//   it('is useful for adding things to arrays', () => {
+//     itemsToLog[0] = 'boink';
+//     assert(returnedVar[0] === 'boink');
+//   })
+// });
 
 
-describe('fetch() mock', function() {
-  const url = 'hello/borp';
-  const fetchPromise = fetch(url); // Will be a promise at this point
-  const requestLog = log.get('fetchRequestLog');
-  const latestRequest = requestLog[requestLog.length - 1];
+// describe('WebAudio mock', function() {
+//   describe('AudioContext Mock', function() {
+//     const ctx = new AudioContext();
+//     it('can create AudioBuffers', async () => {
+//       const arrayBuffer = new ArrayBuffer(8);
+//       const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
+//       assert(audioBuffer instanceof AudioBuffer);
+//     });
+//   });
+// });
 
-  let promiseResolver;
-  const arrayBufferPromise = new Promise(resolve => promiseResolver = resolve);
 
-  it('requests and logs the correct path', () => assert(latestRequest === url));
+// describe('fetch() mock', function() {
+//   const url = 'hello/borp';
+//   const fetchPromise = fetch(url); // Will be a promise at this point
+//   const requestLog = log.get('fetchRequestLog');
+//   const latestRequest = requestLog[requestLog.length - 1];
 
-  it('returns a response we can get an ArrayBuffer from...', async () => {
-    const response = await fetchPromise;
-    const arrayBuffer = await response.arrayBuffer();
-    promiseResolver(arrayBuffer); // To pass this onto later tests...
-    assert(arrayBuffer instanceof ArrayBuffer);
-  });
-});
+//   let promiseResolver;
+//   const arrayBufferPromise = new Promise(resolve => promiseResolver = resolve);
+
+//   it('requests and logs the correct path', () => assert(latestRequest === url));
+
+//   it('returns a response we can get an ArrayBuffer from...', async () => {
+//     const response = await fetchPromise;
+//     const arrayBuffer = await response.arrayBuffer();
+//     promiseResolver(arrayBuffer); // To pass this onto later tests...
+//     assert(arrayBuffer instanceof ArrayBuffer);
+//   });
+// });
