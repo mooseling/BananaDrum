6518dd9fad1971978de4a2c786cd7ffe900b4782
James Stevenson <mooseling@hotmail.com>
2023-01-31 22:45:48 +0000

    (Dev) Get webpack build work (no tests or ts here now)

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const config = {
   mode: 'development',
   entry: {
-    prod: './packages/bananadrum-core/src/prod/index.tsx',
-    test: './packages/bananadrum-core/src/test/index.ts'
+    prod: './index.js',
+    // test: './packages/bananadrum-core/src/test/index.ts'
   },
-  module: {
-    rules: [
-      {
-        test: /\.[tj]sx?/,
-        loader: 'ts-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    extensionAlias: {
-      '.js': ['.js', '.ts'],
-      '.jsx': ['.jsx', '.tsx']
-     }
-  }
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.[tj]sx?/,
+  //       loader: 'ts-loader'
+  //     }
+  //   ]
+  // },
+  // resolve: {
+  //   extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  //   extensionAlias: {
+  //     '.js': ['.js', '.ts'],
+  //     '.jsx': ['.jsx', '.tsx']
+  //    }
+  // }
 };
 
 export default config;
