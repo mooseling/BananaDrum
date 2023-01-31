f5c52ae199fee5900bbbf57db0df92fba4e05e7c
James Stevenson <mooseling@hotmail.com>
2023-01-31 22:43:35 +0000

    [BananaDrumPlayer] Fix

--- packages/bananadrum-core/src/prod/BananaDrumPlayer.ts
+++ packages/bananadrum-core/src/prod/BananaDrumPlayer.ts
@@ -5,6 +5,7 @@ import { BananaDrum, BananaDrumPlayer } from "./types";
 export function createBananaDrumPlayer(bananaDrum:BananaDrum): BananaDrumPlayer {
   const eventEngine = getEventEngine();
   const arrangementPlayer = createArrangementPlayer(bananaDrum.arrangement);
+  eventEngine.connect(arrangementPlayer);
 
   return {bananaDrum, eventEngine, arrangementPlayer};
 }
