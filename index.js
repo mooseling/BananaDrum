32ce61a4139e6ad30f8ae12d3fbe83285a4747cc
James Stevenson <mooseling@hotmail.com>
2023-02-07 19:03:51 +0000

    (Dev) Remove window.React

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-import React from 'react';
 import { createBananaDrum, instrumentCollection, exampleSongString } from 'bananadrum-core';
 import { createBananaDrumPlayer } from 'bananadrum-player';
 import { createBananaDrumUi } from 'bananadrum-ui';
-
-// Set React to global so we don't have to import it in every file with JSX
-// A benefit of this is to supress TS messages about unused var React
-window.React = React;
 
 
 document.getElementById('load-button').addEventListener('click', function() {
