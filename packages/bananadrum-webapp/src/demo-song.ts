3e9b5339e52276c92c1145d966158978cbd4476c
James Stevenson <mooseling@hotmail.com>
2023-09-12 10:30:05 +0100

    New demo song!

--- packages/bananadrum-webapp/src/demo-song.ts
+++ packages/bananadrum-webapp/src/demo-song.ts
@@ -1,1 +1,1 @@
-export const demoSongString = '4-4.110.1.1-4.16.026AE.2j0vp.5GpQPq1.6594h8.71KWH~.8l1Kg.9SLHS.41HjPI.11Jlga';
+export const demoSongString = '4-4.110.1.1-4.16.01bDGD.2kBip.5Gm_CKR.7M43.8Hgm.9SLHS.11IOwQ.3auavauav.63a4oy4';
