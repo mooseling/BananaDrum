dfaf622e6b9a15f34a743e665e308f510a141af7
James Stevenson <james.stevenson@zedify.co.uk>
2024-02-04 14:37:26 +0000

    Export errorLog from banandrum-core

--- packages/bananadrum-core/src/prod/index.ts
+++ packages/bananadrum-core/src/prod/index.ts
@@ -4,5 +4,6 @@ export { getShareLink } from './serialisation.js';
 
 export { createPublisher } from './Publisher.js';
 export { exists, isSameTiming } from './utils.js';
+export { errorLog } from './ErrorLog.js';
 
 export * from './types.js';
