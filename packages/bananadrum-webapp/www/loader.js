8b4dd42efdafd7b4a026ffaf0ffe401144e1a3fa
James Stevenson <mooseling@hotmail.com>
2024-02-10 22:44:42 +0000

    Bump versions

--- packages/bananadrum-webapp/www/loader.js
+++ packages/bananadrum-webapp/www/loader.js
@@ -84,7 +84,7 @@
 
   function loadBananaDrum() {
     const script = document.createElement('script');
-    script.src = '/bundle.js?1.3.1';
+    script.src = '/bundle.js?1.3.2';
     document.body.appendChild(script);
   }
 
