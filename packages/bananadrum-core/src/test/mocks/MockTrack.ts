2d0e5ad09e30a5f4f0843ff742fe7850169c7c97
James Stevenson <mooseling@hotmail.com>
2024-04-08 20:49:44 +0100

    Comment out MockTrack.ts

--- packages/bananadrum-core/src/test/mocks/MockTrack.ts
+++ packages/bananadrum-core/src/test/mocks/MockTrack.ts
@@ -1,18 +1,18 @@
-import { Track } from "../../prod/types.js";
+// import { Track } from "../../prod/types.js";
 
-export function MockTrack(): Track {
-  return {
-    id: '0',
-    arrangement: null,
-    instrument: null,
-    notes: [],
-    polyrhythms: [],
-    getNoteAt: timing => null,
-    addPolyrhythm: (start, end, length) => {},
-    removePolyrhythm: (polyrhythm) => {},
-    colour: 'blue',
-    clear: () => {},
-    subscribe: () => {},
-    unsubscribe: () => {}
-  };
-}
+// export function MockTrack(): Track {
+//   return {
+//     id: '0',
+//     arrangement: null,
+//     instrument: null,
+//     notes: [],
+//     polyrhythms: [],
+//     getNoteAt: timing => null,
+//     addPolyrhythm: (start, end, length) => {},
+//     removePolyrhythm: (polyrhythm) => {},
+//     colour: 'blue',
+//     clear: () => {},
+//     subscribe: () => {},
+//     unsubscribe: () => {}
+//   };
+// }
