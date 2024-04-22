633426f938899edeb2c212ca60fd716d19aaf098
James Stevenson <mooseling@hotmail.com>
2024-04-22 10:31:40 +0100

    <Polyrhythm> Remove test colours

--- packages/bananadrum-ui/src/prod/components/PolyrhythmViewer.tsx
+++ packages/bananadrum-ui/src/prod/components/PolyrhythmViewer.tsx
@@ -1,24 +1,10 @@
 import { Polyrhythm } from "bananadrum-core";
-import { useMemo } from "react";
 import { NoteViewer } from "./NoteViewer";
 
 export function PolyrhythmViewer({polyrhythm}:{polyrhythm:Polyrhythm}): JSX.Element {
-  const colour = useMemo(() => colours[colourIndex++ % colours.length], []);
-
   return (
-    <div id={`polyrhythm-${polyrhythm.id}`} className="polyrhythm-viewer" style={{backgroundColor:colour}}>
+    <div id={`polyrhythm-${polyrhythm.id}`} className="polyrhythm-viewer">
       {polyrhythm.notes.map(note => <NoteViewer note={note} inPolyrhythm={true} key={note.id}/>)}
     </div>
   );
 }
-
-
-let colourIndex = 0;
-
-const colours = [
-  'hsl(268, 61%, 69%)',
-  'hsl(200, 97%, 51%)',
-  'hsl(182, 63%, 34%)',
-  'hsl(25, 80%, 63%)',
-  'hsl(52, 100%, 70%)'
-];
