9ad8da72b9b09d6c98337a7b6b1b69076a91bc7c
James Stevenson <mooseling@hotmail.com>
2024-04-27 18:05:55 +0100

    <Polyrhythm> Handle shrouded case in delete mode

--- packages/bananadrum-ui/src/prod/components/PolyrhythmViewer.tsx
+++ packages/bananadrum-ui/src/prod/components/PolyrhythmViewer.tsx
@@ -5,25 +5,49 @@ import { useSubscription } from "../hooks/useSubscription";
 import { NoteViewer } from "./NoteViewer";
 
 export function PolyrhythmViewer({polyrhythm}:{polyrhythm:Polyrhythm}): JSX.Element {
+  const track = polyrhythm.start.track;
   const modeManager = useContext(ModeManagerContext);
-  const [deleteMode, setDeleteMode] = useState(modeManager.deletePolyrhythmMode);
 
+  const [deleteMode, setDeleteMode] = useState(modeManager.deletePolyrhythmMode);
   useSubscription(modeManager, () => setDeleteMode(modeManager.deletePolyrhythmMode));
+
+  const [isShrouded, setShrouded] = useState(checkShrouded(polyrhythm));
+  useSubscription(track, () => setShrouded(checkShrouded(polyrhythm)));
 
   return (
     <div id={`polyrhythm-${polyrhythm.id}`} className="polyrhythm-viewer">
       {
         deleteMode
         ? (
-          <div className="delete-polyrhythm-wrapper">
-            <button
-              className="push-button"
-              onClick={() => polyrhythm.start.track.removePolyrhythm(polyrhythm)}
-            >Delete</button>
+          <div className={`delete-polyrhythm-wrapper ${isShrouded ? 'shrouded' : ''}`}>
+          {
+            isShrouded
+            ? (<></>)
+            : (
+              <button
+                disabled={isShrouded}
+                className="push-button"
+                onClick={() => track.removePolyrhythm(polyrhythm)}
+              >Delete</button>
+            )
+          }
           </div>
         )
         : polyrhythm.notes.map(note => <NoteViewer note={note} inPolyrhythm={true} key={note.id}/>)
       }
     </div>
   );
+}
+
+
+function checkShrouded(polyrhythm:Polyrhythm) {
+  const track = polyrhythm.start.track;
+  for (const otherPolyrhythm of track.polyrhythms) {
+    if (otherPolyrhythm !== polyrhythm) {
+      if (otherPolyrhythm.start.polyrhythm === polyrhythm || otherPolyrhythm.end.polyrhythm === polyrhythm)
+        return true;
+    }
+  }
+
+  return false;
 }
