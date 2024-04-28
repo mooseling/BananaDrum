49b03805ae7c002accd7dbdb318b06667785ae93
James Stevenson <mooseling@hotmail.com>
2024-04-28 13:10:21 +0100

    [SelectionManager] Reorganise

--- packages/bananadrum-ui/src/prod/components/SelectionControls.tsx
+++ packages/bananadrum-ui/src/prod/components/SelectionControls.tsx
@@ -1,32 +1,35 @@
 import { useContext } from "react";
 import { SelectionManagerContext } from "../BananaDrumUi";
+import { SelectionManager } from "../SelectionManager";
 import { NumberInput } from "./General";
 
+
+
 export function SelectionControls(): JSX.Element {
-
   const selectionManager = useContext(SelectionManagerContext);
-
-  const createPolyrhythm = (inputValue:string) => {
-    const polyrhythmLength = Number(inputValue);
-    if (!polyrhythmLength)
-      return;
-
-    const start = selectionManager.getFirst();
-    const end = selectionManager.getLast();
-    const track = start.track;
-
-    track.addPolyrhythm(start, end, polyrhythmLength);
-    selectionManager.clearSelection();
-  }
 
   return (
     <div className="overlay-wrapper">
       <div className="time-control">
       New number of notes: <NumberInput
           getValue={() => '0'}
-          setValue={createPolyrhythm}
+          setValue={value => createPolyrhythm(value, selectionManager)}
         />
       </div>
     </div>
   );
+}
+
+
+const createPolyrhythm = (inputValue:string, selectionManager:SelectionManager) => {
+  const polyrhythmLength = Number(inputValue);
+  if (!polyrhythmLength)
+    return;
+
+  const start = selectionManager.getFirst();
+  const end = selectionManager.getLast();
+  const track = start.track;
+
+  track.addPolyrhythm(start, end, polyrhythmLength);
+  selectionManager.clearSelection();
 }
