e25edb199bc3e0794c36b939775935f4ed15b36b
James Stevenson <mooseling@hotmail.com>
2024-05-10 17:03:50 +0100

    <TouchHoldDetector> Prevent touch-hold context menu on mobile

--- packages/bananadrum-ui/src/prod/components/TouchHoldDetector.tsx
+++ packages/bananadrum-ui/src/prod/components/TouchHoldDetector.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useRef } from "react";
+import { isMobile } from "../isMobile";
 
 export function TouchHoldDetector(
   {callback, holdLength, children}:{callback:()=>void, holdLength:number, children:JSX.Element}
@@ -13,9 +14,16 @@ export function TouchHoldDetector(
       onTouchStart={onTouchStart}
       onTouchMove={cancel}
       onTouchEnd={cancel}
+      onContextMenu={preventDefault}
       style={{width:"100%", height:"100%"}} // This may not work well in some cases. The approach can be changed if not.
     >
       {children}
     </div>
   );
+}
+
+
+function preventDefault(event:React.MouseEvent) {
+  if (isMobile)
+    event.preventDefault();
 }
