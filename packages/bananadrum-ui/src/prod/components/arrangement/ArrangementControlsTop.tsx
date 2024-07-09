270f214164fe540b21e9c1a9e6b1ea01ebe1de0c
James Stevenson <mooseling@hotmail.com>
2024-07-09 16:49:55 +0100

    <ArrangementControls> Add prototype edit-title button

--- packages/bananadrum-ui/src/prod/components/arrangement/ArrangementControlsTop.tsx
+++ packages/bananadrum-ui/src/prod/components/arrangement/ArrangementControlsTop.tsx
@@ -40,6 +40,18 @@ export function ArrangementControlsTop(): JSX.Element {
       }
       <SmallSpacer />
       <TimeControls arrangement={arrangement} />
+      <button
+        className="push-button medium gray"
+        style={{
+          marginLeft:'12pt',
+          fontSize: '20.5pt',
+          fontFamily: 'serif',
+          textTransform: 'capitalize', // .push-button normally lower-cases contents
+          lineHeight: '1em'
+        }}
+        >
+        T&nbsp;<img src="images/icons/pencil_white.svg" style={{height:'0.78em'}} />
+      </button>
       <SmallSpacer />
       <ExpandingSpacer />
       <ShareButton />
