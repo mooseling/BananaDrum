7e52c304a0fba6565d91c9b17ba701a272510fd7
James Stevenson <mooseling@hotmail.com>
2025-01-22 12:25:40 +0000

    Improve text spacing for bar numbers

--- packages/bananadrum-ui/src/prod/components/guiderail/TimingViewer.tsx
+++ packages/bananadrum-ui/src/prod/components/guiderail/TimingViewer.tsx
@@ -3,6 +3,8 @@ import { Timing } from 'bananadrum-core';
 export function TimingViewer({timing}:{timing:Timing}): JSX.Element {
   const startOfBar = timing.step === 1;
   return (<div className={`guiderail-timing note-width ${startOfBar ? 'start-of-bar' : ''}`}>
-    {startOfBar ? timing.bar : ''}
+    <div className='guiderail-timing-content'>
+      {startOfBar ? timing.bar : ''}
+    </div>
   </div>);
 }
