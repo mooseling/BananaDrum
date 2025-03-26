639d29e06da0d9bdffa8be0051cfdde7c4b2ac44
James Stevenson <mooseling@hotmail.com>
2025-03-26 13:52:48 +0000

    [Pubsub] Move unsubscribeAll to correct type

--- packages/banana-pubsub/src/prod/types.ts
+++ packages/banana-pubsub/src/prod/types.ts
@@ -4,12 +4,12 @@ export type Subscription<T> = (value?:T) => void
 export interface Subscribable<T> {
   subscribe(callback:Subscription<T>): void
   unsubscribe(callback:Subscription<T>): void
-  unsubscribeAll(): void
 }
 
 
 export interface Publisher<T> extends Subscribable<T> {
   publish(value:T): void
+  unsubscribeAll(): void
 }
 
 
