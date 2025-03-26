ab5c9798e7e693faa7690a46d106115b57ada93e
James Stevenson <mooseling@hotmail.com>
2025-03-26 13:25:48 +0000

    Remove legacy Publisher.ts in order to BREAK EVERYTHING

--- packages/bananadrum-core/src/prod/index.ts
+++ packages/bananadrum-core/src/prod/index.ts
@@ -2,7 +2,6 @@ export { createBananaDrum } from './BananaDrum.js';
 export { getLibrary } from './Library.js';
 export { getShareLink } from './serialisation.js';
 
-export { createPublisher } from './Publisher.js';
 export { exists, isSameTiming } from './utils.js';
 export { errorLog } from './ErrorLog.js';
 
