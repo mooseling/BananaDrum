9085f22a65a59582b8b70e5b24728104e97454b2
James Stevenson <mooseling@hotmail.com>
2025-04-08 15:52:42 +0100

    Add comment

--- packages/bananadrum-core/src/prod/types/snapshots.ts
+++ packages/bananadrum-core/src/prod/types/snapshots.ts
@@ -1,3 +1,7 @@
+// The goal here is to have intermediate objects which capture the state of the composition
+// They will be used for share links, undo/redo, and tab state preservation
+// Tab state preservation may require saving objects into history.state, so must be serialisable/simple
+
 export interface ArrangementSnapshot {
   title: string
   timeParams: TimeParamsSnapshot
