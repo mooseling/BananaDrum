a6bbfe5131e7c3add603cd84a42f9b31c14fa027
James Stevenson <mooseling@hotmail.com>
2026-05-07 21:18:29 +0100

    Fix image path on dev server

--- packages/bananadrum-ui/src/prod/components/ShareButton.tsx
+++ packages/bananadrum-ui/src/prod/components/ShareButton.tsx
@@ -4,7 +4,7 @@ export function ShareButton(): JSX.Element {
   return (
     <button id="share-button" className="push-button" onClick={() => toggleOverlay('share', 'show')}>
       <span>Share this beat!</span>
-      <img style={{width:'18pt', height:'18pt'}} src="/images/icons/paper_plane_white.svg" />
+      <img style={{width:'18pt', height:'18pt'}} src="images/icons/paper_plane_white.svg" />
     </button>
   );
 }
